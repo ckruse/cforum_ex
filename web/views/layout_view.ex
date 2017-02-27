eb600028d2507eb23e4a8f66fee57e92e261a4b2
Christian Kruse <cjk@defunct.ch>
2017-02-27 21:35:56 +0100

    feat: framework for the page title

--- web/views/layout_view.ex
+++ web/views/layout_view.ex
@@ -1,3 +1,16 @@
 defmodule Cforum.LayoutView do
   use Cforum.Web, :view
+
+  def page_title(conn, assigns) do
+    try do
+      apply(view_module(conn), :page_title, [action_name(conn), assigns]) <> " — SELFHTML Forum"
+    rescue
+      UndefinedFunctionError -> default_page_title(conn, assigns)
+      FunctionClauseError -> default_page_title(conn, assigns)
+    end
+  end
+
+  def default_page_title(_conn, _assigns) do
+    " — SELFHTML Forum"
+  end
 end
