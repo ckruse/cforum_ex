53847782ba02c0cce3048e5da693327daa2f7a4b
Christian Kruse <cjk@defunct.ch>
2017-02-27 21:31:26 +0100

    feat: humans.txt

--- lib/cforum/endpoint.ex
+++ lib/cforum/endpoint.ex
@@ -9,7 +9,7 @@ defmodule Cforum.Endpoint do
   # when deploying your static files in production.
   plug Plug.Static,
     at: "/", from: :cforum, gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
+    only: ~w(css fonts images js favicon.ico robots.txt humans.txt)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
