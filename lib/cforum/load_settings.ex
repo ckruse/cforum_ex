6b5dec120d40945377b89735dc54805e1e633c11
Christian Kruse <cjk@defunct.ch>
2017-02-28 01:02:58 +0100

    fix: don't fail when a config could be found

--- lib/cforum/load_settings.ex
+++ lib/cforum/load_settings.ex
@@ -16,13 +16,13 @@ defmodule Cforum.Plug.LoadSettings do
 
   def load_user_conf(conn, user) when user == nil, do: conn
   def load_user_conf(conn, user) do
-    conf = Repo.get_by!(Setting, user_id: user.user_id)
+    conf = Repo.get_by(Setting, user_id: user.user_id)
     Plug.Conn.assign(conn, :user_config, conf)
   end
 
   def load_forum_conf(conn, forum) when forum == nil, do: conn
   def load_forum_conf(conn, forum) do
-    conf = Repo.get_by!(Setting, forum_id: forum.forum_id)
+    conf = Repo.get_by(Setting, forum_id: forum.forum_id)
     Plug.Conn.assign(conn, :forum_config, conf)
   end
 
