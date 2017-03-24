38bf3880157f9ea52dd7df31f221d6b5f4f7b196
Christian Kruse <cjk@defunct.ch>
2017-03-24 14:37:57 +0100

    fix: halt request in error case

--- lib/cforum/guardian_error_handler.ex
+++ lib/cforum/guardian_error_handler.ex
@@ -6,17 +6,20 @@ defmodule Cforum.GuardianErrorHandler do
     conn
     |> Phoenix.Controller.put_flash(:error, gettext("You have to be logged in to see this page!"))
     |> Phoenix.Controller.redirect(to: session_path(conn, :new))
+    |> Plug.Conn.halt
   end
 
   def admin_required(conn, _params) do
     conn
     |> Phoenix.Controller.put_flash(:error, gettext("You have to be logged in and an admin to see this page!"))
     |> Phoenix.Controller.redirect(to: session_path(conn, :new))
+    |> Plug.Conn.halt
   end
 
   def access_forbidden(conn, _params) do
     conn
     |> Phoenix.Controller.put_flash(:error, gettext("You don't have access to this page!"))
     |> Phoenix.Controller.redirect(to: session_path(conn, :new))
+    |> Plug.Conn.halt
   end
 end
