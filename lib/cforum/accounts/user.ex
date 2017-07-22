62511280938253059baa98f193f4da540b04c7b7
Christian Kruse <cjk@defunct.ch>
2017-07-22 08:25:53 +0200

    fix: typo

--- lib/cforum/accounts/user.ex
+++ lib/cforum/accounts/user.ex
@@ -75,7 +75,7 @@ defmodule Cforum.Accounts.User do
   defp put_password_hash(changeset) do
     case changeset do
       %Ecto.Changeset{valid?: true, changes: %{password: pass}} ->
-        put_change(changeset, :password_hash, Comeonin.Bcrypt.hashpwsalt(pass))
+        put_change(changeset, :encrypted_password, Comeonin.Bcrypt.hashpwsalt(pass))
       _ ->
         changeset
     end
