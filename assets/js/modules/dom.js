c07537d776c23fab1a98fc841901fbea72fc15fc
CountOrlok <18417130+CountOrlok@users.noreply.github.com>
2017-08-06 21:47:49 +0200

    adds create and parse functions

--- assets/js/modules/dom.js
+++ assets/js/modules/dom.js
@@ -112,6 +112,45 @@ export function classes (selector, context = document) {
 
 
 /**
+ *  @function create
+ *
+ *
+ *  @summary
+ *
+ *  Creates an element.
+ *
+ *
+ *  @description
+ *
+ *  This is a shorthand for the native createElement method,
+ *  so it takes a string with a valid element name and returns
+ *  an element of the specified type, or if the type has not
+ *  been recognized, an unknown element. If it should become
+ *  necessary, a future implementation might add support
+ *  for an optional parameter for custom elements.
+ *
+ *
+ *  @param { string } tagName
+ *
+ *  The type of element to create.
+ *
+ *
+ *  @return { Element }
+ *
+ *  The element created.
+ *
+ *
+ *
+ */
+export function create (tagName) {
+  return document.createElement(tagName);
+}
+
+
+
+
+
+/**
  *  @function id
  *
  *
@@ -205,6 +244,46 @@ export const on = curry(function (type, context, callback, options = false) {
   context.addEventListener(type, callback, options);
   return context;
 });
+
+
+
+
+
+/**
+ *  @function parse
+ *
+ *
+ *  @summary
+ *
+ *  Parses a string with markup into a DocumentFragment.
+ *
+ *
+ *  @description
+ *
+ *  This function expects a string of HTML code. It parses the
+ *  markup into DOM nodes and attaches them to a DocumentFragment
+ *  which is then returned. The fragment can be inserted into the
+ *  DOM of the page like any other node and in this process, it
+ *  is replaced by its content.
+ *
+ *
+ *  @param { string } markup
+ *
+ *  The HTML code to parse.
+ *
+ *
+ *  @return { DocumentFragment }
+ *
+ *  A DocumentFragment whose content is the parsed markup.
+ *
+ *
+ *
+ */
+export function parse (markup) {
+  const fragment = document.createDocumentFragment();
+  fragment.innerHTML = markup;
+  return fragment;
+}
 
 
 
