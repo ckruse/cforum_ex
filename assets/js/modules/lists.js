12daf404ca982d6e0996abfd48fab392312c459a
CountOrlok <18417130+CountOrlok@users.noreply.github.com>
2017-08-08 16:07:57 +0200

    comment for tail function

--- assets/js/modules/lists.js
+++ assets/js/modules/lists.js
@@ -48,6 +48,19 @@ export const head = curry(function head ([first]) {
  *  Returns the last element of a list.
  *
  *
+ *  @description
+ *
+ *
+ *  @param { Array } list
+ *
+ *  An array or other indexed collection.
+ *
+ *
+ *  @return { * }
+ *
+ *  The last item of the list.
+ *
+ *
  *
  */
 export const peak = curry(function peak (list) {
@@ -64,11 +77,31 @@ export const peak = curry(function peak (list) {
  *
  *  @summary
  *
- *  Returns all but the first element of a list.
+ *  Returns the remainder of a list.
+ *
+ *
+ *  @description
+ *
+ *  The tail function takes an array and returns another array
+ *  whose elements are those of the array provided without the first
+ *  element. If the supplied array contains less than two elements,
+ *  an empty array will be returned. The argument does not have to
+ *  be an array, though. It can be any object that implements
+ *  the Iterable interface.
+ *
+ *
+ *  @param { Array } list
+ *
+ *  An array or other object that is iterable.
+ *
+ *
+ *  @return { Array }
+ *
+ *  The remainder of the provided list.
  *
  *
  *
  */
 export const tail = curry(function tail ([head, ...rest]) {
-  return rest.length ? rest : [head];
+  return rest;
 });
