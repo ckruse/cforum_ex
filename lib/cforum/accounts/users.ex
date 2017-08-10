65f7415f2d0c22f20a90a1e42c218d2a7e9ad3c6
Christian Kruse <cjk@defunct.ch>
2017-08-10 16:47:55 +0200

    ref: documentation

--- lib/cforum/accounts/users.ex
+++ lib/cforum/accounts/users.ex
@@ -1,6 +1,6 @@
 defmodule Cforum.Accounts.Users do
   @moduledoc """
-  The boundary for the Accounts system.
+  The boundary for the Accounts.Users system.
   """
 
   import Ecto.Query, warn: false
@@ -10,6 +10,11 @@ defmodule Cforum.Accounts.Users do
 
   @doc """
   Returns the list of users.
+
+  ## Parameters
+
+  - query_params: an option list containing a `order` and a `limit` option,
+    describing the sort order and the offset/limit
 
   ## Examples
 
@@ -24,6 +29,14 @@ defmodule Cforum.Accounts.Users do
     |> Repo.all
   end
 
+  @doc """
+  Returns the number of users
+
+  ## Examples
+
+      iex> count_users()
+      0
+  """
   def count_users do
     User
     |> select(count("*"))
@@ -51,6 +64,20 @@ defmodule Cforum.Accounts.Users do
     |> Repo.one!
   end
 
+  @doc """
+  Gets a single user.
+
+  Returns `nil` if the User does not exist.
+
+  ## Examples
+
+      iex> get_user(123)
+      %User{}
+
+      iex> get_user(456)
+      nil
+
+  """
   def get_user(id) do
     from(u in User,
       preload: [:settings, :badges, [badges_users: :badge]],
@@ -58,6 +85,23 @@ defmodule Cforum.Accounts.Users do
     |> Repo.one
   end
 
+  @doc """
+  Gets the first user matching in either the username or the email column
+
+  Returns nil if no user could be found
+
+  ## Examples
+
+      iex> get_user_user_by_username_or_email("Luke")
+      %User{}
+
+      iex> get_user_user_by_username_or_email("luke@example.org")
+      %User{}
+
+      iex> get_user_user_by_username_or_email("luke@aldebaran.gov")
+      nil
+
+  """
   def get_user_by_username_or_email(login) do
     from(user in User,
       preload: [:settings, :badges, [badges_users: :badge]],
@@ -67,6 +111,20 @@ defmodule Cforum.Accounts.Users do
     |> Repo.one
   end
 
+  @doc """
+  Gets a single user by a `reset_password_token`.
+
+  Raises `Ecto.NoResultsError` if the User does not exist.
+
+  ## Examples
+
+      iex> get_user_by_reset_password_token!("aoeuaoeu")
+      %User{}
+
+      iex> get_user_by_reset_password_token!("nonexistant")
+      ** (Ecto.NoResultsError)
+
+  """
   def get_user_by_reset_password_token!(reset_token) do
     Repo.get_by!(User, reset_password_token: reset_token)
   end
@@ -107,6 +165,18 @@ defmodule Cforum.Accounts.Users do
     |> Repo.update()
   end
 
+  @doc """
+  Updates the password of a user.
+
+  ## Examples
+
+      iex> update_user_password(user, %{password: new_value, password_confirmation: new_value})
+      {:ok, %User{}}
+
+      iex> update_user_password(user, %{password: new_value, password_confirmation: other_value})
+      {:error, %Ecto.Changeset{}}
+
+  """
   def update_user_password(%User{} = user, attrs) do
     user
     |> User.password_changeset(attrs)
@@ -117,6 +187,18 @@ defmodule Cforum.Accounts.Users do
     |> Repo.update()
   end
 
+  @doc """
+  Updates the `reset_password_token` of a user.
+
+  ## Examples
+
+      iex> update_user_password(user, %{reset_password_token: "111", reset_password_sent_at: Timex.now})
+      {:ok, %User{}}
+
+      iex> update_user_password(user, %{reset_password_token: nil, reset_password_sent_at: Timex.now})
+      {:error, %Ecto.Changeset{}}
+
+  """
   def update_user_reset_password_token(%User{} = user, attrs) do
     user
     |> User.reset_password_token_changeset(attrs)
@@ -152,16 +234,49 @@ defmodule Cforum.Accounts.Users do
     User.changeset(user, %{})
   end
 
+  @doc """
+  Returns an `%Ecto.Changeset{}` for tracking user password changes.
+
+  ## Examples
+
+      iex> change_user_password(user)
+      %Ecto.Changeset{source: %User{}}
+
+  """
   def change_user_password(%User{} = user) do
     User.password_changeset(user, %{})
   end
 
+  @doc """
+  Registers a new user (creates an uncorfirmed new user)
+
+  ## Examples
+
+    iex> register_user(%{field: value})
+    {:ok, %User{}}
+
+    iex> register_user(%{field: bad_value})
+    {:error, %Ecto.Changeset{}}
+
+  """
   def register_user(attrs) do
     %User{active: true}
     |> User.register_changeset(attrs)
     |> Repo.insert()
   end
 
+  @doc """
+  Confirms an unconfirmed user
+
+  ## Examples
+
+    iex> confirm_user("auaoeu")
+    {:ok, %User{}}
+
+    iex> confirm_user("nonexistant")
+    {:error, nil}
+
+  """
   def confirm_user(token) do
     case Repo.get_by(User, confirmation_token: token) do
       nil ->
@@ -171,6 +286,15 @@ defmodule Cforum.Accounts.Users do
     end
   end
 
+  @doc """
+  Generates a new `reset_password_token` for a user
+
+  ## Examples
+
+    iex> get_reset_password_token(%User{})
+    %User{}
+
+  """
   def get_reset_password_token(user) do
     token = :crypto.strong_rand_bytes(32)
     |> Base.encode64
@@ -182,6 +306,15 @@ defmodule Cforum.Accounts.Users do
     user
   end
 
+  @doc """
+  Returns a list of unique badges for a user
+
+  ## Examples
+
+    iex> unique_badges(user)
+    [%Badge{}, ...]
+
+  """
   def unique_badges(user) do
     user.badges_users
     |> Enum.reduce(%{}, fn(bu, acc) ->
@@ -193,12 +326,35 @@ defmodule Cforum.Accounts.Users do
     |> Enum.sort(&(&1[:times] >= &2[:times]))
   end
 
+  @doc """
+  Returns the configuration value for the directive specified by `config_name`; if the
+  user is nil or has no settings of that name it returns the default value from
+  the `Cforum.ConfigManager`
+
+  ## Examples
+
+    iex> conf(%User{}, "foo")
+    "yes"
+
+  """
+  def conf(user, config_name)
   def conf(nil, name), do: Cforum.ConfigManager.defaults[name]
   def conf(%User{settings: nil}, name), do: Cforum.ConfigManager.defaults[name]
   def conf(%User{settings: settings}, name) do
     settings.options[name] || Cforum.ConfigManager.defaults[name]
   end
 
+  @doc """
+  Authenticates a user specified by the login (email or username) and the password
+
+  ## Examples
+
+      iex> authenticate_user("luke", "foobar")
+      {:ok, %User{}}
+
+      iex> authenticate_user("luke", "wrong")
+      {:error, %Ecto.Changeset{}}
+  """
   def authenticate_user(login, password) do
     user = get_user_by_username_or_email(login)
 
@@ -218,10 +374,30 @@ defmodule Cforum.Accounts.Users do
     end
   end
 
+  @doc """
+  Returns true if the user has a badge of the type specified by `badge`
+
+  ## Examples
+      iex> has_badge?(%User{}, "seo_profi")
+      true
+
+      iex> has_badge?(%User{}, "seo_profi")
+      false
+  """
   def has_badge?(user, badge) do
     Enum.find(user.badges, &(&1.badge_type == badge)) != nil
   end
 
+  @doc """
+  Returns true if the user is a moderator (in any forum)
+
+  ## Examples
+      iex> moderator?(%User{admin: true})
+      true
+
+      iex> moderator?(%User{})
+      false
+  """
   def moderator?(%User{admin: true}), do: true
   def moderator?(user) do
     # TODO
