4cd80cae12498e8d3ab06278435ec2be4d31cdc7
Christian Kruse <cjk@defunct.ch>
2017-08-11 16:45:47 +0200

    fix: always set an order; set it to fallback when no order is given

--- lib/cforum/order_api.ex
+++ lib/cforum/order_api.ex
@@ -1,8 +1,11 @@
 defmodule Cforum.OrderApi do
   import Ecto.Query, warn: false
 
-  def set_ordering(query, nil, _), do: query
-  def set_ordering(query, [], _), do: query
+  def set_ordering(query, nil, fallback_order), do: set_ordering(query, [], fallback_order)
+  def set_ordering(query, [], fallback_order) do
+    query
+    |> order_by(^fallback_order)
+  end
   def set_ordering(query, ordering, fallback_order) do
     query
     |> order_by(^ordering)
