100ce2d2d9de474e24874d544307dd2d997530ab
Orlok <matthias.fulde@gmx.de>
2017-08-15 12:43:28 +0200

    adds function parse to elements module

--- assets/js/modules/elements.js
+++ assets/js/modules/elements.js
@@ -71,3 +71,44 @@ export const children = curry(function children (element) {
 export function create (tagName) {
   return document.createElement(tagName);
 }
+
+
+
+
+
+/**
+ *  @function parse
+ *
+ *
+ *  @summary
+ *
+ *  Parses a string with markup into a DocumentFragment.
+ *
+ *
+ *  @description
+ *
+ *  This function expects a string of HTML code. It parses the
+ *  markup into DOM nodes and attaches them to a DocumentFragment
+ *  which is then returned. The fragment can be inserted into the
+ *  DOM of the page like any other node and in this process, it
+ *  is replaced by its content.
+ *
+ *
+ *  @param { string } markup
+ *
+ *  The HTML code to parse.
+ *
+ *
+ *  @return { DocumentFragment }
+ *
+ *  A DocumentFragment whose content is the parsed markup.
+ *
+ *
+ *
+ */
+export function parse (markup) {
+  const fragment = document.createDocumentFragment();
+
+  fragment.innerHTML = markup;
+  return fragment;
+}
