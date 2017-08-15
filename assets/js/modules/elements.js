6e36bcb1614c34f05780f0c7d501343b8ead4d89
Orlok <matthias.fulde@gmx.de>
2017-08-15 12:42:30 +0200

    adds function create to elements module

--- assets/js/modules/elements.js
+++ assets/js/modules/elements.js
@@ -32,3 +32,42 @@ import { curry } from './functional.js';
 export const children = curry(function children (element) {
   return Array.from(element.children);
 });
+
+
+
+
+
+/**
+ *  @function create
+ *
+ *
+ *  @summary
+ *
+ *  Creates an element.
+ *
+ *
+ *  @description
+ *
+ *  This is a shorthand for the native createElement method,
+ *  so it takes a string with a valid element name and returns
+ *  an element of the specified type, or if the type has not
+ *  been recognized, an unknown element. If it should become
+ *  necessary, a future implementation might add support
+ *  for an optional parameter for custom elements.
+ *
+ *
+ *  @param { string } tagName
+ *
+ *  The type of element to create.
+ *
+ *
+ *  @return { Element }
+ *
+ *  The element created.
+ *
+ *
+ *
+ */
+export function create (tagName) {
+  return document.createElement(tagName);
+}
