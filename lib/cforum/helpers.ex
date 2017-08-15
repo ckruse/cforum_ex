c6e1458f9c1725d797bb1c1a46b356668eca3dd8
Christian Kruse <cjk@defunct.ch>
2017-08-15 07:57:54 +0200

    fix: we can’t pattern match an empty map with `%{}`

--- lib/cforum/helpers.ex
+++ lib/cforum/helpers.ex
@@ -4,6 +4,6 @@ defmodule Cforum.Helpers do
   def blank?(0), do: true
   def blank?(false), do: true
   def blank?([]), do: true
-  def blank?(%{}), do: true
+  def blank?(map) when map == %{}, do: true
   def blank?(_), do: false
 end
