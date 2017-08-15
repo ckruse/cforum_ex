4e769bf71b89c58a668714738a2c21ef95691bb4
CountOrlok <18417130+CountOrlok@users.noreply.github.com>
2017-08-15 13:10:37 +0200

    updates imports to tabs module

--- assets/js/modules/tabs.js
+++ assets/js/modules/tabs.js
@@ -29,7 +29,7 @@
 
 
 
-import { children, firstElementChild, lastElementChild, nextElementSibling, parentElement, previousElementSibling } from './elements.js';
+import { children, firstElementChild, focus, lastElementChild, nextElementSibling, parentElement, previousElementSibling } from './elements.js';
 
 import { bind, key, preventDefault, ready } from './events.js'
 
