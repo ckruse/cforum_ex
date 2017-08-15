a098df3f35839be150657be060f6077c4cdb0534
Orlok <matthias.fulde@gmx.de>
2017-08-15 12:38:42 +0200

    removes function children from dom module

--- assets/js/modules/dom.js
+++ assets/js/modules/dom.js
@@ -26,25 +26,6 @@ import { curry } from './functional.js';
 
 
 /**
- *  @function children
- *
- *
- *  @summary
- *
- *  Returns a list with child elements.
- *
- *
- *
- */
-export const children = curry(function children (element) {
-  return Array.from(element.children);
-});
-
-
-
-
-
-/**
  *  @function create
  *
  *
