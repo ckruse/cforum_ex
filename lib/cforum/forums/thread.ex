a170d1331c9041dd8b1979c686cfdd7d9981e5a6
Christian Kruse <cjk@defunct.ch>
2017-08-15 07:58:57 +0200

    ref: default values for empty `attribs` attributes

--- lib/cforum/forums/thread.ex
+++ lib/cforum/forums/thread.ex
@@ -21,7 +21,7 @@ defmodule Cforum.Forums.Thread do
     field :sorted_messages, :any, virtual: true
     field :tree, :any, virtual: true
     field :accepted, :any, virtual: true
-    field :attribs, :map, virtual: true
+    field :attribs, :map, virtual: true, default: %{}
 
     timestamps(inserted_at: :created_at)
   end
