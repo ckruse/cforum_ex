8f4bb79a8f55035ecbc6669d2b29ef5d677b60a1
Christian Kruse <cjk@defunct.ch>
2017-08-25 07:43:28 +0200

    fix: better error message and redirect to root for logged in users on 403

--- lib/cforum_web/error_handler.ex
+++ lib/cforum_web/error_handler.ex
@@ -2,6 +2,8 @@ defmodule CforumWeb.ErrorHandler do
   import CforumWeb.Router.Helpers
   import CforumWeb.Gettext
 
+  alias Cforum.Accounts.User
+
   def unauthenticated(conn, _params) do
     conn
     |> Phoenix.Controller.put_flash(:error, gettext("You have to be logged in to see this page!"))
@@ -13,6 +15,13 @@ defmodule CforumWeb.ErrorHandler do
     conn
     |> Phoenix.Controller.put_flash(:error, gettext("You have to be logged in and an admin to see this page!"))
     |> Phoenix.Controller.redirect(to: session_path(conn, :new))
+    |> Plug.Conn.halt
+  end
+
+  def access_forbidden(%Plug.Conn{assigns: %{current_user: %User{}}} = conn, _params) do
+    conn
+    |> Phoenix.Controller.put_flash(:error, gettext("You don't have access to this page!"))
+    |> Phoenix.Controller.redirect(to: forum_path(conn, :index))
     |> Plug.Conn.halt
   end
 
