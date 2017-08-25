6fab294a8f7a8c81cfe2f9e8148e7a3d901ec795
Christian Kruse <cjk@defunct.ch>
2017-08-25 08:13:48 +0200

    fix: ensure that the notification we’re trying to look at belongs to the user

--- lib/cforum_web/controllers/notification_controller.ex
+++ lib/cforum_web/controllers/notification_controller.ex
@@ -3,6 +3,8 @@ defmodule CforumWeb.NotificationController do
 
   alias Cforum.Accounts.Notifications
 
+  plug :check_access
+
   def index(conn, params) do
     {sort_params, conn} = sort_collection(conn, [:created_at, :subject, :is_read])
     count = Notifications.count_notifications(conn.assigns[:current_user])
@@ -16,8 +18,8 @@ defmodule CforumWeb.NotificationController do
     render(conn, "index.html", notifications: notifications, paging: paging)
   end
 
-  def show(conn, %{"id" => id}) do
-    notification = Notifications.get_notification!(id)
+  def show(conn, %{"id" => _id}) do
+    notification = conn.assigns[:notification]
 
     # we ignore errors in this case; the user doesn't care, he just want's to
     # go to the referenced subject
@@ -25,8 +27,8 @@ defmodule CforumWeb.NotificationController do
     redirect(conn, to: notification.path)
   end
 
-  def update_unread(conn, %{"notification_id" => id}) do
-    notification = Notifications.get_notification!(id)
+  def update_unread(conn, %{"notification_id" => _id}) do
+    notification = conn.assigns[:notification]
 
     case Notifications.update_notification(notification, %{is_read: false}) do
       {:ok, _notification} ->
@@ -40,12 +42,22 @@ defmodule CforumWeb.NotificationController do
     end
   end
 
-  def delete(conn, %{"id" => id}) do
-    notification = Notifications.get_notification!(id)
-    Notifications.delete_notification(notification)
+  def delete(conn, %{"id" => _id}) do
+    Notifications.delete_notification(conn.assigns[:notification])
 
     conn
     |> put_flash(:info, gettext("Notification deleted successfully."))
     |> redirect(to: notification_path(conn, :index))
   end
+
+  defp check_access(%Plug.Conn{params: %{"id" => id}} = conn, _) do
+    notification = Notifications.get_notification!(id)
+
+    if conn.assigns[:current_user] == nil || conn.assigns[:current_user].user_id != notification.recipient_id do
+      CforumWeb.ErrorHandler.access_forbidden(conn, nil)
+    else
+      assign(conn, :notification, notification)
+    end
+  end
+  defp check_access(conn, _), do: conn
 end
