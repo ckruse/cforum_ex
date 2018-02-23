979ccac15b8bd9e26e2282cca452154c90fa4d8b
Christian Kruse <cjk@defunct.ch>
2018-02-23 14:30:28 +0100

    ref: meta information for invisible threads and subscriptions

--- lib/cforum_web/views/messages/subscription_view.ex
+++ lib/cforum_web/views/messages/subscription_view.ex
@@ -1,3 +1,8 @@
 defmodule CforumWeb.Messages.SubscriptionView do
   use CforumWeb, :view
+
+  def page_title(:index, _), do: gettext("subscribed messages")
+  def page_heading(action, assigns), do: page_title(action, assigns)
+  def body_id(:index, _), do: "subscribed-messages-list"
+  def body_classes(:index, _), do: "subscribed-messages list"
 end
--- lib/cforum_web/views/threads/invisible_view.ex
+++ lib/cforum_web/views/threads/invisible_view.ex
@@ -1,3 +1,8 @@
 defmodule CforumWeb.Threads.InvisibleView do
   use CforumWeb, :view
+
+  def page_title(:index, _), do: gettext("invisible threads")
+  def page_heading(action, assigns), do: page_title(action, assigns)
+  def body_id(:index, _), do: "invisible-threads-list"
+  def body_classes(:index, _), do: "invisible-threads list"
 end
