e80c0ceee7b777fecc28be53ba3c80815e505a85
Christian Kruse <cjk@defunct.ch>
2018-03-11 08:02:09 +0100

    feat: button helpers for generating links

--- lib/cforum_web/views/helpers/button.ex
+++ lib/cforum_web/views/helpers/button.ex
@@ -198,6 +198,174 @@ defmodule CforumWeb.Views.Helpers.Button do
 
     {form, opts}
   end
+
+  @doc """
+  Generates a button with a link for not very specific actions
+
+  `opts` is a keyword list, the only special values is `:to` defining
+  the target URL. Every other attribute will be set as an attribute on
+  the generated &lt;a&gt; tag.
+
+  ### Examples
+
+      default_button_link("foo", to: "bar")
+      => <a href="bar" class="cf-btn">foo</a>
+
+      default_button_link("foo", to: "bar", class: "lulu")
+      => <a href="bar" class="cf-btn lulu">foo</a>
+
+      default_button_link(to: "bar") do
+        {:safe, "<b>foo</b>"}
+      end
+      => <a href="bar" class="cf-btn"><b>foo</b></a>
+  """
+  def default_button_link(opts, do: contents),
+    do: link(Keyword.update(opts, :class, "cf-btn", &(&1 <> " cf-btn")), do: contents)
+
+  def default_button_link(text, opts), do: link(text, Keyword.update(opts, :class, "cf-btn", &(&1 <> " cf-btn")))
+
+  @doc """
+  Generates a button with a link for not very specific actions for an
+  index view, e.g. a table with a list of relations; see
+  `default_button_link/2`.
+  """
+  def default_index_button_link(opts, do: contents),
+    do: link(Keyword.update(opts, :class, "cf-index-btn", &(&1 <> " cf-index-btn")), do: contents)
+
+  def default_index_button_link(text, opts),
+    do: link(text, Keyword.update(opts, :class, "cf-index-btn", &(&1 <> " cf-index-btn")))
+
+  @doc """
+  Generates a button with a link for a primary action; see
+  `default_button_link/2`.
+  """
+  def primary_button_link(opts, do: contents),
+    do: link(Keyword.update(opts, :class, "cf-primary-btn", &(&1 <> " cf-primary-btn")), do: contents)
+
+  def primary_button_link(text, opts),
+    do: link(text, Keyword.update(opts, :class, "cf-primary-btn", &(&1 <> " cf-primary-btn")))
+
+  @doc """
+  Generates a button with a link for a primary action for an index
+  view, e.g. a table with a list of relations; see
+  `default_button_link/2`.
+  """
+  def primary_index_button_link(opts, do: contents),
+    do: link(Keyword.update(opts, :class, "cf-primary-index-btn", &(&1 <> " cf-primary-index-btn")), do: contents)
+
+  def primary_index_button_link(text, opts),
+    do: link(text, Keyword.update(opts, :class, "cf-primary-index-btn", &(&1 <> " cf-primary-index-btn")))
+
+  @doc """
+  Generates a button with a link for a destructive action; see
+  `default_button_link/2`. Additionally it sets a `data-cf-confirm`
+  attribute the UI can intervene on and make sure that the user will
+  be asked for confirmation.
+  """
+  def destructive_button_link(opts, do: contents) do
+    opts =
+      opts
+      |> Keyword.update(:class, "cf-destructive-btn", &(&1 <> " cf-destructive-btn"))
+      |> Keyword.update(:"data-cf-confirm", gettext("Are you sure?"), & &1)
+
+    link(opts, do: contents)
+  end
+
+  def destructive_button_link(text, opts) do
+    opts =
+      opts
+      |> Keyword.update(:class, "cf-destructive-btn", &(&1 <> " cf-destructive-btn"))
+      |> Keyword.update(:"data-cf-confirm", gettext("Are you sure?"), & &1)
+
+    link(text, opts)
+  end
+
+  @doc """
+  Generates a button with a link for a destructive action for an index
+  view, e.g. a table with a list of relations; see
+  `destructive_button_link/2`.
+  """
+  def destructive_index_button_link(opts, do: contents) do
+    opts =
+      opts
+      |> Keyword.update(:class, "cf-destructive-btn", &(&1 <> " cf-destructive-btn"))
+      |> Keyword.update(:"data-cf-confirm", gettext("Are you sure?"), & &1)
+
+    link(opts, do: contents)
+  end
+
+  def destructive_index_button_link(text, opts) do
+    opts =
+      opts
+      |> Keyword.update(:class, "cf-destructive-btn", &(&1 <> " cf-destructive-btn"))
+      |> Keyword.update(:"data-cf-confirm", gettext("Are you sure?"), & &1)
+
+    link(text, opts)
+  end
+
+  @doc """
+  Generates a submit button for not very specific actions
+
+  `opts` is a keyword list, every attribute will be set as an
+  attribute on the generated &lt;button&gt; tag.
+
+  ### Examples
+
+      default_submit("foo")
+      => <button type="submit" class="cf-btn">foo</a>
+
+      default_button_link("foo", foo: "bar")
+      => <button type="submit" foo="bar" class="cf-btn">foo</a>
+
+      default_button_link(foo: "bar") do
+        {:safe, "<b>foo</b>"}
+      end
+      => <button type="submit" class="cf-btn" foo="bar"><b>foo</b></a>
+  """
+  def default_submit(_, opts \\ [])
+
+  def default_submit(opts, do: contents),
+    do: submit(Keyword.update(opts, :class, "cf-btn", &(&1 <> " cf-btn")), do: contents)
+
+  def default_submit(text, opts), do: submit(text, Keyword.update(opts, :class, "cf-btn", &(&1 <> " cf-btn")))
+
+  @doc """
+  Generates a submit button for a primary action; see
+  `default_submit/2`.
+  """
+  def primary_submit(_, opts \\ [])
+
+  def primary_submit(opts, do: contents),
+    do: submit(Keyword.update(opts, :class, "cf-primary-btn", &(&1 <> " cf-primary-btn")), do: contents)
+
+  def primary_submit(text, opts),
+    do: submit(text, Keyword.update(opts, :class, "cf-primary-btn", &(&1 <> " cf-primary-btn")))
+
+  @doc """
+  Generates a submit button for a destructive action; see
+  `default_submit/2`. Additionally it sets a `data-cf-confirm`
+  attribute the UI can intervene on and make sure that the user will
+  be asked for confirmation.
+  """
+  def destructive_submit(_, opts \\ [])
+
+  def destructive_submit(opts, do: contents) do
+    opts =
+      opts
+      |> Keyword.update(:class, "cf-destructive-btn", &(&1 <> " cf-destructive-btn"))
+      |> Keyword.update(:"data-cf-confirm", gettext("Are you sure?"), & &1)
+
+    submit(opts, do: contents)
+  end
+
+  def destructive_submit(text, opts) do
+    opts =
+      opts
+      |> Keyword.update(:class, "cf-destructive-btn", &(&1 <> " cf-destructive-btn"))
+      |> Keyword.update(:"data-cf-confirm", gettext("Are you sure?"), & &1)
+
+    submit(text, opts)
+  end
 end
 
 # eof
