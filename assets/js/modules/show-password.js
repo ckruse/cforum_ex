64658eb6db7f671b90b39f68d18ad46c34e1461d
Christian Kruse <cjk@defunct.ch>
2018-03-14 08:55:21 +0100

    feat: added show password buttons to password fields

---
+++ assets/js/modules/show-password.js
@@ -0,0 +1,38 @@
+import { ready, bind, preventDefault } from "./events.js";
+import { all } from "./selectors.js";
+import { when } from "./logic.js";
+import { create, nextElementSibling, parentElement, getAttribute, setAttribute, focus } from "./elements.js";
+import { compose, pipe } from "./functional.js";
+import { equal } from "./predicates.js";
+
+function toggleInputType(input) {
+  if (getAttribute("type", input) == "password") {
+    setAttribute("type", "text", input);
+  } else {
+    setAttribute("type", "password", input);
+  }
+
+  focus(input);
+}
+
+function setupShowPassword(passwords) {
+  passwords.forEach(input => {
+    const text = input.dataset.showPassword || "show password";
+    const anchor = create("button");
+
+    anchor.textContent = text;
+    anchor.classList.add("cf-show-password");
+    setAttribute("type", "button", anchor);
+
+    bind(anchor, {
+      click: pipe(preventDefault, () => input, toggleInputType)
+    });
+
+    parentElement(input).insertBefore(anchor, nextElementSibling(input));
+  });
+}
+
+ready(function() {
+  const passwords = all("[data-show-password]");
+  when(() => passwords.length, setupShowPassword, passwords);
+});
