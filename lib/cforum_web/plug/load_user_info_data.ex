ee5ab359faccd21a7c4c635302429a56d21fd5de
Christian Kruse <cjk@defunct.ch>
2018-04-03 15:01:18 +0200

    ref: code formatting

--- lib/cforum_web/plug/load_user_info_data.ex
+++ lib/cforum_web/plug/load_user_info_data.ex
@@ -15,14 +15,8 @@ defmodule CforumWeb.Plug.LoadUserInfoData do
 
       user ->
         conn
-        |> Plug.Conn.assign(
-             :unread_notifications,
-             Accounts.Notifications.count_notifications(user, true)
-           )
-        |> Plug.Conn.assign(
-             :unread_mails,
-             Accounts.PrivMessages.count_priv_messages(user, true)
-           )
+        |> Plug.Conn.assign(:unread_notifications, Accounts.Notifications.count_notifications(user, true))
+        |> Plug.Conn.assign(:unread_mails, Accounts.PrivMessages.count_priv_messages(user, true))
     end
   end
 end
