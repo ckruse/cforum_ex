0a302e03a1112f994844ac6e6552a7c7f8727916
Christian Kruse <ckruse@giftzwerg.localdomain>
2018-04-24 07:28:37 +0200

    ref: missing body ids and classes

--- lib/cforum_web/views/admin/group_view.ex
+++ lib/cforum_web/views/admin/group_view.ex
@@ -10,9 +10,14 @@ defmodule CforumWeb.Admin.GroupView do
   def body_id(:index, _), do: "admin-groups-index"
   def body_id(:new), do: "admin-groups-new"
   def body_id(:create), do: "admin-groups-create"
+  def body_id(:edit), do: "admin-groups-edit"
+  def body_id(:update), do: "admin-groups-update"
 
   def body_classes(:index, _), do: "admin groups index"
   def body_classes(:new, _), do: "admin groups new"
+  def body_classes(:create, _), do: "admin groups create"
+  def body_classes(:edit, _), do: "admin groups edit"
+  def body_classes(:update, _), do: "admin groups update"
 
   def page_heading(action, assigns), do: page_title(action, assigns)
 end
