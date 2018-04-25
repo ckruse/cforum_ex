b22c16d9768d2e257ca6177176dc34aa83231b6e
Christian Kruse <cjk@defunct.ch>
2018-04-25 15:44:42 +0200

    ref: typos and temporary workarounds

--- lib/cforum/abilities/thread.ex
+++ lib/cforum/abilities/thread.ex
@@ -1,7 +1,8 @@
 defmodule Cforum.Abilities.Thread do
   defmacro __using__(_opts) do
     quote do
-      def may?(conn, "thread", :index, _), do: access_forum?(conn.assigns[:current_user], conn.assigns[:current_forum])
+      # TODO implement proper rights
+      def may?(conn, "thread", _, _), do: access_forum?(conn.assigns[:current_user], conn.assigns[:current_forum])
       def may?(conn, "threads/invisible", _, _), do: signed_in?(conn)
       def may?(conn, "threads/open_close", _, _), do: signed_in?(conn)
     end
