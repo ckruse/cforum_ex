e816f3618b18203df15c0a1bdc031a1b6189cb6a
Christian Kruse <cjk@defunct.ch>
2018-06-10 09:16:27 +0200

    ref: bare controllers for retag and flag

---
+++ lib/cforum_web/controllers/messages/retag_controller.ex
@@ -0,0 +1,11 @@
+defmodule CforumWeb.Messages.RetagController do
+  use CforumWeb, :controller
+
+  def edit(conn, _params) do
+    conn
+  end
+
+  def update(conn, _params) do
+    conn
+  end
+end
