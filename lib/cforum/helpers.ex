1e4443d95e0522b2e6989dded629287f0e2580a1
Christian Kruse <cjk@defunct.ch>
2018-06-11 20:13:37 +0200

    fix: not loaded associations are blank

--- lib/cforum/helpers.ex
+++ lib/cforum/helpers.ex
@@ -14,6 +14,7 @@ defmodule Cforum.Helpers do
   def blank?(0), do: true
   def blank?(false), do: true
   def blank?([]), do: true
+  def blank?(%Ecto.Association.NotLoaded{}), do: true
   def blank?(map) when map == %{}, do: true
   def blank?(_), do: false
 
