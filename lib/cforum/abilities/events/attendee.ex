d8629964451493a8c5a3b060a8faa5679a67d521
Christian Kruse <cjk@defunct.ch>
2018-06-17 07:35:34 +0200

    feat: first version of the events submodule

--- lib/cforum/abilities/events/attendee.ex
+++ lib/cforum/abilities/events/attendee.ex
@@ -1,13 +1,24 @@
 defmodule Cforum.Abilities.Events.Attendee do
   defmacro __using__(_opts) do
     quote do
-      def may?(conn, "events/attendee", action, _) when action in [:new, :create],
-        do: present?(conn.assigns[:event]) && conn.assigns[:event].visible == true
+      alias Cforum.Events
 
-      def may?(conn, "events/attendee", _, resource) do
+      def may?(conn, "events/attendee", action, _) when action in [:new, :create] do
+        base = present?(conn.assigns[:event]) && Events.open?(conn.assigns.event)
+
+        if signed_in?(conn) do
+          base &&
+            Enum.find_value(conn.assigns.event.attendees, &(&1.user_id == conn.assigns.current_user.user_id)) == nil
+        else
+          base
+        end
+      end
+
+      def may?(conn, "events/attendee", _, attendee) do
+        attendee = attendee || conn.assigns[:attendee]
+
         admin?(conn) ||
-          (signed_in?(conn) && present?(conn.assigns[:attendee]) &&
-             conn.assigns[:attendee].user_id == conn.assigns[:current_user].user_id)
+          (signed_in?(conn) && present?(attendee) && attendee.user_id == conn.assigns[:current_user].user_id)
       end
     end
   end
--- lib/cforum_web/controllers/events/attendee_controller.ex
+++ lib/cforum_web/controllers/events/attendee_controller.ex
@@ -2,18 +2,60 @@ defmodule CforumWeb.Events.AttendeeController do
   use CforumWeb, :controller
 
   alias Cforum.Events
-  alias Cforum.Events.Attendee
+  alias Cforum.Events.{Attendee, Attendees}
 
   def new(conn, _params) do
-    changeset = Events.change_attendee(%Attendee{})
+    changeset = Attendees.change_attendee(%Attendee{})
     render(conn, "new.html", changeset: changeset)
   end
 
-  def create(conn, _params) do
+  def create(conn, %{"attendee" => attendee_params}) do
+    case Attendees.create_attendee(conn.assigns.event, conn.assigns.current_user, attendee_params) do
+      {:ok, _attendee} ->
+        conn
+        |> put_flash(:info, gettext("You registered successfully."))
+        |> redirect(to: event_path(conn, :show, conn.assigns.event))
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        render(conn, "new.html", changeset: changeset)
+    end
+  end
+
+  def edit(conn, _params) do
+    changeset = Attendees.change_attendee(conn.assigns.attendee)
+    render(conn, "edit.html", changeset: changeset)
+  end
+
+  def update(conn, %{"attendee" => attendee_params}) do
+    case Attendees.update_attendee(conn.assigns.attendee, attendee_params) do
+      {:ok, _attendee} ->
+        conn
+        |> put_flash(:info, gettext("Your registration was successfully updated."))
+        |> redirect(to: event_path(conn, :show, conn.assigns.event))
+
+      {:error, changeset} ->
+        render(conn, "edit.html", changeset: changeset)
+    end
+  end
+
+  def delete(conn, _params) do
+    {:ok, _attendee} = Attendees.delete_attendee(conn.assigns.attendee)
+
+    conn
+    |> put_flash(:info, gettext("Attendee deleted successfully."))
+    |> redirect(to: event_path(conn, :show, conn.assigns.event))
   end
 
   def load_resource(conn) do
     event = Events.get_event!(conn.params["event_id"])
-    Plug.Conn.assign(conn, :event, event)
+
+    attendee =
+      if Phoenix.Controller.action_name(conn) in [:edit, :update, :delete],
+        do: Attendees.get_attendee!(event, conn.params["id"]),
+        else: nil
+
+    conn
+    |> Plug.Conn.assign(:event, event)
+    |> Plug.Conn.assign(:attendee, attendee)
   end
 end
