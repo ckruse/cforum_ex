a9fdb29f25b8d7e4fd7778c3ca544d2ed2002fc2
Christian Kruse <christian@kruse.cool>
2018-07-15 15:16:21 +0200

    more testing
    - accept controller
    - flag controller
    - interesting controller
    - mark read controller

---
+++ test/cforum_web/controllers/messages/accept_controller_test.exs
@@ -0,0 +1,40 @@
+defmodule CforumWeb.Messages.AcceptControllerTest do
+  use CforumWeb.ConnCase
+
+  setup [:setup_tests]
+
+  alias Cforum.Forums.Messages
+
+  test "accepts an answer", %{conn: conn, forum: forum, thread: thread, message: message} do
+    conn = post(conn, accept_message_path(conn, thread, message), f: forum.slug, r: "message")
+    assert redirected_to(conn) == message_path(conn, :show, thread, message)
+    assert get_flash(conn, :info) == gettext("Message has successfully been accepted as a solving answer")
+  end
+
+  test "responds with 403 on an already accepted answer", %{conn: conn, user: user, thread: thread, message: message} do
+    Messages.accept_message(message, user, 15)
+    conn = post(conn, accept_message_path(conn, thread, message))
+    assert html_response(conn, 403)
+  end
+
+  test "unaccepts an answer", %{conn: conn, user: user, forum: forum, thread: thread, message: message} do
+    Messages.accept_message(message, user, 15)
+    conn = post(conn, unaccept_message_path(conn, thread, message), f: forum.slug, r: "message")
+    assert redirected_to(conn) == message_path(conn, :show, thread, message)
+    assert get_flash(conn, :info) == gettext("Message has successfully been unaccepted as a solving answer")
+  end
+
+  test "responds with 403 on an not accepted answer", %{conn: conn, thread: thread, message: message} do
+    conn = post(conn, unaccept_message_path(conn, thread, message))
+    assert html_response(conn, 403)
+  end
+
+  defp setup_tests(%{conn: conn}) do
+    user = build(:user) |> as_admin() |> insert
+
+    forum = insert(:public_forum)
+    thread = insert(:thread, forum: forum)
+    message = insert(:message, forum: forum, thread: thread)
+    {:ok, user: user, conn: login(conn, user), thread: thread, message: message, forum: forum}
+  end
+end
---
+++ test/cforum_web/controllers/messages/interesting_controller_test.exs
@@ -0,0 +1,59 @@
+defmodule CforumWeb.Messages.InterestingControllerTest do
+  use CforumWeb.ConnCase
+
+  setup [:setup_tests]
+
+  alias Cforum.Forums.Messages
+
+  describe "listing" do
+    test "lists all interesting messages", %{conn: conn, user: user, message: message} do
+      Messages.mark_message_interesting(user, message)
+      conn = get(conn, interesting_path(conn, :index))
+      assert html_response(conn, 200) =~ gettext("messages marked as interesting")
+      assert html_response(conn, 200) =~ message.subject
+    end
+
+    test "doesn't list boring messages", %{conn: conn, message: message} do
+      conn = get(conn, interesting_path(conn, :index))
+      assert html_response(conn, 200) =~ gettext("messages marked as interesting")
+      refute html_response(conn, 200) =~ message.subject
+    end
+  end
+
+  describe "marking as interesting" do
+    test "marks as interesting", %{conn: conn, forum: forum, thread: thread, message: message} do
+      conn = post(conn, interesting_message_path(conn, thread, message), f: forum.slug, r: "message")
+      assert redirected_to(conn) == message_path(conn, :show, thread, message)
+      assert get_flash(conn, :info) == gettext("Message was successfully marked as interesting.")
+    end
+
+    test "responds with 403 on interesting messages", %{conn: conn, user: user, thread: thread, message: message} do
+      Messages.mark_message_interesting(user, message)
+      conn = post(conn, interesting_message_path(conn, thread, message))
+      assert html_response(conn, 403)
+    end
+  end
+
+  describe "marking as boring" do
+    test "marks as boring", %{conn: conn, user: user, forum: forum, thread: thread, message: message} do
+      Messages.mark_message_interesting(user, message)
+      conn = post(conn, boring_message_path(conn, thread, message), f: forum.slug, r: "message")
+      assert redirected_to(conn) == message_path(conn, :show, thread, message)
+      assert get_flash(conn, :info) == gettext("Interesting mark was successfully removed.")
+    end
+
+    test "responds with 403 on already boring messages", %{conn: conn, thread: thread, message: message} do
+      conn = post(conn, boring_message_path(conn, thread, message))
+      assert html_response(conn, 403)
+    end
+  end
+
+  defp setup_tests(%{conn: conn}) do
+    user = build(:user) |> insert
+
+    forum = insert(:public_forum)
+    thread = insert(:thread, forum: forum)
+    message = insert(:message, forum: forum, thread: thread)
+    {:ok, user: user, conn: login(conn, user), thread: thread, message: message, forum: forum}
+  end
+end
