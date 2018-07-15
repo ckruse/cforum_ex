3e34bbf2a1457ad09c2c46630939d48192095176
Christian Kruse <christian@kruse.cool>
2018-07-15 14:36:57 +0200

    ref: more testing (subscription controller & vote controller)

---
+++ test/cforum_web/controllers/messages/subscription_controller_test.exs
@@ -0,0 +1,59 @@
+defmodule CforumWeb.Messages.SubscriptionControllerTest do
+  use CforumWeb.ConnCase
+
+  setup [:setup_tests]
+
+  alias Cforum.Forums.Messages
+
+  describe "listing" do
+    test "lists all subscribed messages", %{conn: conn, user: user, message: message} do
+      Messages.subscribe_message(user, message)
+      conn = get(conn, subscription_path(conn, :index))
+      assert html_response(conn, 200) =~ gettext("subscribed messages")
+      assert html_response(conn, 200) =~ message.subject
+    end
+
+    test "doesn't list not subscribed messages", %{conn: conn, message: message} do
+      conn = get(conn, subscription_path(conn, :index))
+      assert html_response(conn, 200) =~ gettext("subscribed messages")
+      refute html_response(conn, 200) =~ message.subject
+    end
+  end
+
+  describe "subscribing" do
+    test "subscribes messages", %{conn: conn, forum: forum, thread: thread, message: message} do
+      conn = post(conn, subscribe_message_path(conn, thread, message), f: forum.slug, r: "message")
+      assert redirected_to(conn) == message_path(conn, :show, thread, message)
+      assert get_flash(conn, :info) == gettext("Message was successfully subscribed.")
+    end
+
+    test "responds with 403 on already subscribed messages", %{conn: conn, thread: thread, message: message} do
+      conn = post(conn, subscribe_message_path(conn, thread, message))
+      conn = post(conn, subscribe_message_path(conn, thread, message))
+      assert html_response(conn, 403)
+    end
+  end
+
+  describe "unsubscribing" do
+    test "unsubscribes messages", %{conn: conn, user: user, forum: forum, thread: thread, message: message} do
+      Messages.subscribe_message(user, message)
+      conn = post(conn, unsubscribe_message_path(conn, thread, message), f: forum.slug, r: "message")
+      assert redirected_to(conn) == message_path(conn, :show, thread, message)
+      assert get_flash(conn, :info) == gettext("Message was successfully unsubscribed.")
+    end
+
+    test "doesn't fail on not subscribed messages", %{conn: conn, thread: thread, message: message} do
+      conn = post(conn, unsubscribe_message_path(conn, thread, message))
+      assert html_response(conn, 403)
+    end
+  end
+
+  defp setup_tests(%{conn: conn}) do
+    user = build(:user) |> insert
+
+    forum = insert(:public_forum)
+    thread = insert(:thread, forum: forum)
+    message = insert(:message, forum: forum, thread: thread)
+    {:ok, user: user, conn: login(conn, user), thread: thread, message: message, forum: forum}
+  end
+end
---
+++ test/cforum_web/controllers/messages/vote_controller_test.exs
@@ -0,0 +1,71 @@
+defmodule CforumWeb.Messages.VoteControllerTest do
+  use CforumWeb.ConnCase
+
+  setup [:setup_tests]
+
+  alias Cforum.Forums.Messages
+
+  describe "upvote" do
+    test "votes up", %{conn: conn, thread: thread, message: message, forum: forum} do
+      conn = post(conn, upvote_message_path(conn, thread, message, f: forum.slug, r: "message"))
+      assert redirected_to(conn) == message_path(conn, :show, thread, message)
+      assert get_flash(conn, :info) == gettext("Successfully upvoted message")
+    end
+
+    test "takes back a vote", %{conn: conn, thread: thread, message: message, forum: forum} do
+      conn = post(conn, upvote_message_path(conn, thread, message))
+      conn = post(conn, upvote_message_path(conn, thread, message, f: forum.slug, r: "message"))
+      assert redirected_to(conn) == message_path(conn, :show, thread, message)
+      assert get_flash(conn, :info) == gettext("Successfully took back vote")
+    end
+  end
+
+  describe "downvote" do
+    test "votes down", %{conn: conn, thread: thread, message: message, forum: forum} do
+      conn = post(conn, downvote_message_path(conn, thread, message, f: forum.slug, r: "message"))
+      assert redirected_to(conn) == message_path(conn, :show, thread, message)
+      assert get_flash(conn, :info) == gettext("Successfully downvoted message")
+    end
+
+    test "takes back a vote", %{conn: conn, thread: thread, message: message, forum: forum} do
+      conn = post(conn, downvote_message_path(conn, thread, message))
+      conn = post(conn, downvote_message_path(conn, thread, message, f: forum.slug, r: "message"))
+      assert redirected_to(conn) == message_path(conn, :show, thread, message)
+      assert get_flash(conn, :info) == gettext("Successfully took back vote")
+    end
+  end
+
+  describe "inverting" do
+    test "takes back a an upvote and creates a downvote", %{conn: conn, thread: thread, message: message, forum: forum} do
+      conn = post(conn, upvote_message_path(conn, thread, message))
+      conn = post(conn, downvote_message_path(conn, thread, message, f: forum.slug, r: "message"))
+      assert redirected_to(conn) == message_path(conn, :show, thread, message)
+      assert get_flash(conn, :info) == gettext("Successfully downvoted message")
+
+      message = Messages.get_message!(message.message_id)
+      assert message.upvotes == 0
+      assert message.downvotes == 1
+    end
+
+    test "takes back a downvote and creates an upvote", %{conn: conn, thread: thread, message: message, forum: forum} do
+      conn = post(conn, downvote_message_path(conn, thread, message))
+      conn = post(conn, upvote_message_path(conn, thread, message, f: forum.slug, r: "message"))
+      assert redirected_to(conn) == message_path(conn, :show, thread, message)
+      assert get_flash(conn, :info) == gettext("Successfully upvoted message")
+
+      message = Messages.get_message!(message.message_id)
+      assert message.upvotes == 1
+      assert message.downvotes == 0
+    end
+  end
+
+  defp setup_tests(%{conn: conn}) do
+    user = build(:user) |> as_admin() |> insert
+    Cforum.Accounts.Scores.create_score(%{user_id: user.user_id, value: 1000})
+
+    forum = insert(:public_forum)
+    thread = insert(:thread, forum: forum)
+    message = insert(:message, forum: forum, thread: thread)
+    {:ok, user: user, conn: login(conn, user), thread: thread, message: message, forum: forum}
+  end
+end
