97d600c7271c67978c5e039f6bd491e071446725
Christian Kruse <christian@kruse.cool>
2018-08-18 16:32:48 +0200

    feat: we now have a job for generating forum stats

---
+++ lib/cforum/forums/forum_stats_job.ex
@@ -0,0 +1,12 @@
+defmodule Cforum.Forums.ForumStatsJob do
+  import Ecto.Query, warn: false
+  require Logger
+
+  alias Cforum.Repo
+  alias Cforum.Forums.Forum
+
+  def gen_stats do
+    from(Forum, select: fragment("gen_forum_stats(forum_id::integer)"))
+    |> Repo.all()
+  end
+end
