f800b2e407054acb50fc6394ac2a0ca6f032a624
Christian Kruse <christian@kruse.cool>
2018-10-06 19:43:22 +0200

    feat: now we are able to show alerts to users

--- assets/js/alerts.js
+++ assets/js/alerts.js
@@ -1,3 +1,6 @@
+import { parse } from "./modules/helpers";
+import { t } from "./modules/i18n";
+
 document.addEventListener("DOMContentLoaded", () => {
   const elem = document.querySelector("#alerts-container");
   elem.addEventListener("click", event => {
@@ -11,3 +14,24 @@ document.addEventListener("DOMContentLoaded", () => {
     alerts.forEach(alrt => alrt.remove());
   }, 10000);
 });
+
+export const alert = (type, text) => {
+  const node = document.createElement("div");
+  node.classList.add("cf-alert", "cf-" + type, "fade", "in");
+
+  const btn = parse(`      <button type="button" class="close" data-dismiss="cf-alert" aria-label="${t("close")}">
+        <span aria-hidden="true">&times;</span>
+      </button>`);
+
+  node.appendChild(btn);
+  node.appendChild(document.createTextNode(text));
+
+  document.querySelector("#alerts-container").appendChild(node);
+
+  if (type == "success") {
+    window.setTimeout(() => node.remove(), 10000);
+  }
+};
+
+export const alertError = text => alert("error", text);
+export const alertSuccess = text => alert("success", text);
