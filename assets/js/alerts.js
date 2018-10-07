f22bf8e5c9c0b5162dcdd62801cfaccd522afc31
Christian Kruse <christian@kruse.cool>
2018-10-07 07:42:22 +0200

    ref: alerts are now a react component

--- assets/js/alerts.js
+++ assets/js/alerts.js
@@ -1,37 +1,83 @@
-import { parse } from "./modules/helpers";
-import { t } from "./modules/i18n";
+import React from "react";
+import { render } from "react-dom";
+import { CSSTransitionGroup } from "react-transition-group";
 
+import { t } from "./modules/i18n";
+import { uniqueId } from "./modules/helpers";
+
+class AlertsContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { alerts: [...this.props.existingAlerts] };
+
+    this.state.alerts.filter(alrt => alrt.type == "success").forEach(alrt => {
+      window.setTimeout(() => this.removeAlert(alrt), 10000);
+    });
+  }
+
+  removeAlert(alert) {
+    this.setState({ ...this.state, alerts: this.state.alerts.filter(alrt => alrt.id != alert.id) });
+  }
+
+  addAlert(alert) {
+    const id = alert.id || uniqueId();
+    const alrtWithId = { ...alert, id: id };
+
+    this.setState({
+      ...this.state,
+      alerts: [...this.state.alerts, alrtWithId]
+    });
+
+    if (alrtWithId.type == "success") {
+      window.setTimeout(() => this.removeAlert(alrtWithId), 10000);
+    }
+  }
+
+  render() {
+    return (
+      <div id="alerts-container">
+        <CSSTransitionGroup transitionName="fade-in" transitionEnterTimeout={300} transitionLeaveTimeout={300}>
+          {this.state.alerts.map(alert => (
+            <div
+              key={alert.id}
+              className={`cf-${alert.type} cf-alert fade in`}
+              role="alert"
+              onClick={() => this.removeAlert(alert)}
+            >
+              <button type="button" className="close" aria-label={t("close")}>
+                <span aria-hidden="true">&times;</span>
+              </button>
+              {alert.text}
+            </div>
+          ))}
+        </CSSTransitionGroup>
+      </div>
+    );
+  }
+}
+
+let alertsContainer = null;
 document.addEventListener("DOMContentLoaded", () => {
   const elem = document.querySelector("#alerts-container");
-  elem.addEventListener("click", event => {
-    if (event.target.matches(".cf-alert button, .cf-alert button span, .cf-alert")) {
-      event.target.closest(".cf-alert").remove();
-    }
+  const existingAlerts = Array.from(elem.querySelectorAll(".cf-alert")).map(alert => {
+    return {
+      id: uniqueId(),
+      type: alert.classList.contains("cf-error") ? "error" : "success",
+      text: alert.querySelector("button").nextSibling.textContent
+    };
   });
 
-  const alerts = elem.querySelectorAll(".cf-alert.cf-success");
-  window.setTimeout(() => {
-    alerts.forEach(alrt => alrt.remove());
-  }, 10000);
+  render(
+    <AlertsContainer
+      ref={cnt => {
+        alertsContainer = cnt;
+      }}
+      existingAlerts={existingAlerts}
+    />,
+    elem
+  );
 });
 
-export const alert = (type, text) => {
-  const node = document.createElement("div");
-  node.classList.add("cf-alert", "cf-" + type, "fade", "in");
-
-  const btn = parse(`      <button type="button" class="close" data-dismiss="cf-alert" aria-label="${t("close")}">
-        <span aria-hidden="true">&times;</span>
-      </button>`);
-
-  node.appendChild(btn);
-  node.appendChild(document.createTextNode(text));
-
-  document.querySelector("#alerts-container").appendChild(node);
-
-  if (type == "success") {
-    window.setTimeout(() => node.remove(), 10000);
-  }
-};
-
+export const alert = (type, text) => alertsContainer.addAlert({ type, text });
 export const alertError = text => alert("error", text);
 export const alertSuccess = text => alert("success", text);
