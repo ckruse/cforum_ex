279b05e77c929d1f8b5bcf4791c8f3991d49cefd
Christian Kruse <christian@kruse.cool>
2018-10-07 17:18:09 +0200

    feat: update last visit for users

---
+++ lib/cforum_web/plug/update_last_visit.ex
@@ -0,0 +1,17 @@
+defmodule CforumWeb.Plug.UpdateLastVisit do
+  @moduledoc """
+  This plug is plugged in the browser pipeline and loads and assigns the
+  current user
+  """
+
+  alias Cforum.Accounts.Users
+
+  def init(opts), do: opts
+
+  def call(conn, _opts) do
+    if conn.assigns[:current_user],
+      do: Task.start(fn -> Users.update_last_visit(conn.assigns[:current_user]) end)
+
+    conn
+  end
+end
