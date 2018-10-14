fa911eacee47dea84b68348e35f1d62da2071dd8
Christian Kruse <christian@kruse.cool>
2018-10-14 09:10:13 +0200

    ref: using load_resource framework now for messages

--- lib/cforum_web/controllers/message_controller.ex
+++ lib/cforum_web/controllers/message_controller.ex
@@ -5,8 +5,6 @@ defmodule CforumWeb.MessageController do
   alias Cforum.Forums.Threads
 
   def show(conn, params) do
-    {thread, message} = get_message(conn, params)
-
     # parameter overwrites cookie overwrites config; validation
     # overwrites everything
 
@@ -16,20 +14,18 @@ defmodule CforumWeb.MessageController do
       |> validate_readmode
 
     if read_mode == "nested",
-      do: Messages.mark_messages_read(conn.assigns[:current_user], thread.messages),
-      else: Messages.mark_messages_read(conn.assigns[:current_user], message)
+      do: Messages.mark_messages_read(conn.assigns[:current_user], conn.assigns.thread.messages),
+      else: Messages.mark_messages_read(conn.assigns[:current_user], conn.assigns.message)
 
     conn
     |> maybe_put_readmode(params, read_mode)
-    |> render("show-#{read_mode}.html", thread: thread, message: message, read_mode: read_mode)
+    |> render("show-#{read_mode}.html", read_mode: read_mode)
   end
 
   def new(conn, params) do
-    {thread, message} = get_message(conn, params)
-
     changeset =
       Messages.new_message_changeset(
-        message,
+        conn.assigns.parent,
         conn.assigns[:current_user],
         conn.assigns[:visible_forums],
         strip_signature: uconf(conn, "quote_signature") != "yes",
@@ -43,31 +39,33 @@ defmodule CforumWeb.MessageController do
         std_replacement: gettext("all")
       )
 
-    render(conn, "new.html", thread: thread, parent: message, changeset: changeset)
+    render(conn, "new.html", changeset: changeset)
   end
 
   def create(conn, %{"message" => message_params} = params) do
-    {thread, parent} = get_message(conn, params)
-
     if Map.has_key?(params, "preview"),
-      do: show_preview(conn, message_params, thread, parent),
-      else: create_message(conn, message_params, thread, parent)
+      do: show_preview(conn, message_params, conn.assigns.thread),
+      else: create_message(conn, message_params, conn.assigns.thread)
   end
 
-  defp show_preview(conn, params, thread, parent) do
-    {message, changeset} = Messages.preview_message(params, conn.assigns[:current_user], thread, parent)
-    render(conn, "new.html", thread: thread, parent: parent, message: message, changeset: changeset, preview: true)
+  defp show_preview(conn, params, thread) do
+    {message, changeset} = Messages.preview_message(params, conn.assigns[:current_user], thread, conn.assigns.parent)
+    render(conn, "new.html", message: message, changeset: changeset, preview: true)
   end
 
-  defp create_message(conn, params, thread, parent) do
-    case Messages.create_message(params, conn.assigns[:current_user], conn.assigns[:visible_forums], thread, parent) do
+  defp create_message(conn, params, thread) do
+    cu = conn.assigns[:current_user]
+    vis_forums = conn.assigns.visible_forums
+    parent = conn.assigns.parent
+
+    case Messages.create_message(params, cu, vis_forums, thread, parent) do
       {:ok, message} ->
         conn
         |> put_flash(:info, gettext("Message created successfully."))
         |> redirect(to: message_path(conn, :show, thread, message))
 
       {:error, changeset} ->
-        render(conn, "new.html", thread: thread, parent: parent, changeset: changeset)
+        render(conn, "new.html", changeset: changeset)
     end
   end
 
@@ -126,6 +124,27 @@ defmodule CforumWeb.MessageController do
     if present?(params["rm"]) && blank?(conn.assigns[:current_user]),
       do: put_resp_cookie(conn, "cf_readmode", read_mode, max_age: 360 * 24 * 60 * 60),
       else: conn
+  end
+
+  def load_resource(conn) do
+    case action_name(conn) do
+      act when act in [:show, :edit, :update] ->
+        {thread, message} = get_message(conn, conn.params)
+
+        conn
+        |> assign(:thread, thread)
+        |> assign(:message, message)
+
+      act when act in [:new, :create] ->
+        {thread, message} = get_message(conn, conn.params)
+
+        conn
+        |> assign(:thread, thread)
+        |> assign(:parent, message)
+
+      _ ->
+        conn
+    end
   end
 
   # TODO implement proper rights
