6754b45c85a9f7bb6f30f728d3ee588861a2c901
Christian Kruse <christian@kruse.cool>
2018-10-20 20:41:30 +0200

    ref: better readable code

--- lib/cforum_web/plug/load_resource.ex
+++ lib/cforum_web/plug/load_resource.ex
@@ -2,8 +2,10 @@ defmodule CforumWeb.Plug.LoadResource do
   def init(opts), do: opts
 
   def call(conn, _opts) do
-    if Keyword.has_key?(Phoenix.Controller.controller_module(conn).__info__(:functions), :load_resource),
-      do: apply(Phoenix.Controller.controller_module(conn), :load_resource, [conn]),
+    controller = Phoenix.Controller.controller_module(conn)
+
+    if Keyword.has_key?(controller.__info__(:functions), :load_resource),
+      do: apply(controller, :load_resource, [conn]),
       else: conn
   end
 end
