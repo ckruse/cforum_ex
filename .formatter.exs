1cc7a4f70c1bef39b9e112b15b9c76b95877ac85
Christian Kruse <christian@kruse.cool>
2018-11-10 19:14:38 +0100

    ref: updating formatter file to match the new phx.new generators

--- .formatter.exs
+++ .formatter.exs
@@ -1,5 +1,6 @@
 [
-  inputs: ["lib/**/*.{ex,exs}", "mix.exs", "config/*.exs", "test/**/*.{ex,exs}"],
+  import_deps: [:phoenix],
+  inputs: ["*.{ex,exs}", "{config,lib,priv,test}/**/*.{ex,exs}"],
   line_length: 120,
   rename_deprecated_at: "1.4.5"
 ]
