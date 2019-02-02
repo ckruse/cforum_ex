4522b647f11590d5631a2f6750d338c9373d323f
Christian Kruse <cjk@defunct.ch>
2019-02-02 14:04:32 +0100

    ref: reduce the number of HTTP requests by waiting for 400ms before querying

--- assets/js/components/editor/mentions.js
+++ assets/js/components/editor/mentions.js
@@ -1,20 +1,28 @@
 import { queryString } from "../../modules/helpers";
 
+let tm = null;
+
 const MentionsReplacements = {
   trigger: "@",
   type: "mention",
   data: (term, callback) => {
+    if (tm) {
+      window.clearTimeout(tm);
+    }
+
     if (term.length <= 0) {
       return [];
     }
 
-    const qs = queryString({ s: term, self: "no", prefix: "yes" });
-    fetch(`/api/v1/users?${qs}`, { credentials: "same-origin" })
-      .then(response => response.json())
-      .then(json => {
-        const users = json.map(u => ({ id: u.user_id, display: "@" + u.username }));
-        callback(users);
-      });
+    tm = window.setTimeout(() => {
+      const qs = queryString({ s: term, self: "no", prefix: "yes" });
+      fetch(`/api/v1/users?${qs}`, { credentials: "same-origin" })
+        .then(response => response.json())
+        .then(json => {
+          const users = json.map(u => ({ id: u.user_id, display: "@" + u.username }));
+          callback(users);
+        });
+    }, 400);
   }
 };
 
