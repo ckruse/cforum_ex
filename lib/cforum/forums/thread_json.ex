a100986d14f982779574deeb0a9336a23553d529
Christian Kruse <christian@kruse.cool>
2019-03-21 20:41:55 +0100

    ref: nicer code

--- lib/cforum/forums/thread_json.ex
+++ lib/cforum/forums/thread_json.ex
@@ -2,20 +2,19 @@ alias Cforum.Forums.Thread
 
 defimpl Jason.Encoder, for: Thread do
   def encode(thread, options) do
-    Jason.Encode.map(
-      %{
-        thread_id: thread.thread_id,
-        archived: thread.archived,
-        deleted: thread.deleted,
-        sticky: thread.sticky,
-        flags: thread.flags,
-        latest_message: thread.latest_message,
-        slug: thread.slug,
-        forum_id: thread.forum_id,
-        created_at: thread.created_at,
-        updated_at: thread.updated_at
-      },
-      options
-    )
+    thread
+    |> Map.take([
+      :thread_id,
+      :archived,
+      :deleted,
+      :sticky,
+      :flags,
+      :latest_message,
+      :slug,
+      :forum_id,
+      :created_at,
+      :updated_at
+    ])
+    |> Jason.Encode.map(options)
   end
 end
