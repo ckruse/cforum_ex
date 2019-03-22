7810b246c0f297a95fce5391377b0ede07c5ee75
Christian Kruse <cjk@defunct.ch>
2019-03-22 11:40:36 +0100

    ref: now using CfPostingForm for the complete form for new messages and threads

--- assets/js/messages/postingform.js
+++ assets/js/messages/postingform.js
@@ -1,9 +1,14 @@
 import React from "react";
 import { render } from "react-dom";
-import CfContentForm from "../components/contentform";
+import CfPostingForm from "../components/postingform";
+
+const cancel = ev => {
+  ev.preventDefault();
+  document.location.href = "/";
+};
 
 const setupContentForms = () => {
-  document.querySelectorAll(".cf-content-form").forEach(el => {
+  document.querySelectorAll(".cf-posting-form").forEach(el => {
     const area = el.querySelector("textarea");
     const tags = Array.from(el.querySelectorAll('input[data-tag="yes"]'))
       .filter(t => !!t.value)
@@ -22,7 +27,33 @@ const setupContentForms = () => {
       globalTagsError = globalTagsErrorElement.textContent;
     }
 
-    render(<CfContentForm text={area.value} name={area.name} tags={tags} globalTagsError={globalTagsError} />, el);
+    const csrfInfo = document.querySelector("meta[name='csrf-token']");
+    const subject = el.querySelector("input[name='message[subject]']").value;
+    const authorNode = el.querySelector("input[name='message[author]']");
+    const author = authorNode ? authorNode.value : "";
+    const email = el.querySelector("input[name='message[email]']").value;
+    const homepage = el.querySelector("input[name='message[homepage]']").value;
+    const problematicSite = el.querySelector("input[name='message[problematic_site]']").value;
+
+    render(
+      <CfPostingForm
+        form={el}
+        subject={subject}
+        author={author}
+        email={email}
+        homepage={homepage}
+        text={area.value}
+        tags={tags}
+        globalTagsError={globalTagsError}
+        problematicSite={problematicSite}
+        csrfInfo={{
+          param: csrfInfo.getAttribute("csrf-param"),
+          token: csrfInfo.getAttribute("content")
+        }}
+        onCancel={cancel}
+      />,
+      el
+    );
   });
 };
 
