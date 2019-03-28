5e91692dcf87966f67f8a6cafd3f3b0ec13b5836
Christian Kruse <cjk@defunct.ch>
2019-03-28 15:46:17 +0100

    ref: cleaning up the database

--- test/cforum/system/clean_counter_tables_job_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule Cforum.System.CleanCounterTablesJobTest do
-  use Cforum.DataCase
-
-  test "cleans counter tables" do
-    insert(:forum)
-    Cforum.System.CleanCounterTablesJob.clean_tables()
-  end
-end
