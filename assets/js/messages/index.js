69bbab52bf03f888a13f00b53a08887e5491afae
Christian Kruse <christian@kruse.cool>
2019-04-04 21:20:54 +0200

    fix: chunks and voting js

--- assets/js/messages/index.js
+++ assets/js/messages/index.js
@@ -1,18 +1,22 @@
 if (document.body.dataset.controller === "MessageController") {
   if (document.body.dataset.action === "show") {
-    import(/* webpackChunkName: "messages" */ "./inline_forms").then(({ default: showInlineForm }) => {
+    import(/* webpackChunkName: "messages-show" */ "./inline_forms").then(({ default: showInlineForm }) => {
       document.querySelectorAll('[data-action="answer"]').forEach(el => el.addEventListener("click", showInlineForm));
     });
+
+    import(/* webpackChunkName: "messages-show" */ "./voting");
   }
 
   if (document.body.classList.contains("nested-view")) {
-    import(/* webpackChunkName: "messages" */ "./nested_view").then(({ default: initNestedView }) => initNestedView());
+    import(/* webpackChunkName: "messages-show" */ "./nested_view").then(({ default: initNestedView }) =>
+      initNestedView()
+    );
   }
 }
 
 if (["MessageController", "ThreadController"].includes(document.body.dataset.controller)) {
   if (["new", "edit", "update", "create"].includes(document.body.dataset.action)) {
-    import(/* webpackChunkName: "messages" */ "./postingform").then(({ default: setupContentForms }) =>
+    import(/* webpackChunkName: "messages-threads" */ "./postingform").then(({ default: setupContentForms }) =>
       setupContentForms()
     );
   }
