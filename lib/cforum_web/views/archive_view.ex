17c997e9b8065d824af225d83623ff9d788a7cba
Christian Kruse <christian@kruse.cool>
2019-04-14 08:12:34 +0200

    ref: updated archive view, based on a proposal of Rolf

--- lib/cforum_web/views/archive_view.ex
+++ lib/cforum_web/views/archive_view.ex
@@ -16,4 +16,23 @@ defmodule CforumWeb.ArchiveView do
   def body_classes(:years, _), do: "archive years"
   def body_classes(:months, _), do: "archive months"
   def body_classes(:threads, _), do: "archive threads"
+
+  def year_title([]), do: ""
+  def year_title([%NaiveDateTime{} = year | _]), do: to_string(year.year)
+
+  def month_link_title(month) do
+    month_name = Timex.format!(month, "%B", :strftime)
+
+    [
+      String.slice(month_name, 0..2),
+      {:safe, "<span class=\"full-month\">"},
+      String.slice(month_name, 3..-1),
+      {:safe, "</span>"}
+    ]
+  end
+
+  def months_for_year(year) do
+    year_no = List.first(year).year
+    for m <- 1..12, do: {%Date{year: year_no, month: m, day: 1}, Enum.find(year, &(&1.month == m)) != nil}
+  end
 end
