e438cc1072574397f144d1b136759321e9578624
Christian Kruse <cjk@defunct.ch>
2019-06-17 07:33:57 +0200

    ref: try to delete more spam users

--- lib/cforum/accounts/user_cleanup_job.ex
+++ lib/cforum/accounts/user_cleanup_job.ex
@@ -10,9 +10,34 @@ defmodule Cforum.Accounts.UserCleanupJob do
 
   @decorate transaction()
   def cleanup do
+    cleanup_unconfirmed_users()
+    cleanup_users_wo_posts()
+  end
+
+  @decorate transaction_event()
+  def cleanup_unconfirmed_users() do
     from(user in User,
-      where: is_nil(user.confirmed_at) and user.confirmation_sent_at <= datetime_add(^DateTime.utc_now(), -24, "hour")
+      where: is_nil(user.confirmed_at),
+      where: fragment("? + interval '24 hours'", user.confirmation_sent_at) <= ^DateTime.utc_now()
     )
+    |> delete_users()
+  end
+
+  @decorate transaction_event()
+  def cleanup_users_wo_posts() do
+    from(user in User,
+      where: is_nil(user.last_visit) or fragment("? + interval '30 days'", user.last_visit) <= ^DateTime.utc_now(),
+      where:
+        fragment(
+          "NOT EXISTS(SELECT message_id FROM messages WHERE messages.user_id = ? and messages.deleted = false)",
+          user.user_id
+        )
+    )
+    |> delete_users()
+  end
+
+  defp delete_users(query) do
+    query
     |> Repo.all()
     |> Enum.each(fn user ->
       Logger.info("Automatically deleting user #{user.username}")
