25f228b37f7ee6d65efe393b3a3af0771da307e0
Christian Kruse <christian@kruse.cool>
2019-06-20 07:55:20 +0200

    ref: after ecto.setup you should have a working database

--- mix.exs
+++ mix.exs
@@ -85,7 +85,7 @@ defmodule Cforum.Mixfile do
   # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     [
-      "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
+      "ecto.setup": ["ecto.create", "ecto.load", "migrate.cf4", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       "ecto.init": ["ecto.create", "ecto.load"],
       "migrate.cf4": [
