0557c2ffecf83eaab71f52e7969d752d71faa134
Christian Kruse <cjk@defunct.ch>
2019-06-21 14:47:02 +0200

    don’t import appsignal.exs

--- config/config.exs
+++ config/config.exs
@@ -44,5 +44,3 @@ config :cforum, Cforum.Scheduler,
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env()}.exs"
-
-import_config "appsignal.exs"
