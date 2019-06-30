f7e34253f9f1c67702121abac6dce80c14162b4b
Christian Kruse <cjk@defunct.ch>
2019-06-30 20:42:30 +0200

    fix: typo in checking access rights for accepts

--- lib/cforum_web/controllers/api/v1/messages/accept_controller.ex
+++ lib/cforum_web/controllers/api/v1/messages/accept_controller.ex
@@ -37,6 +37,7 @@ defmodule CforumWeb.Api.V1.Messages.AcceptController do
     thread =
       Threads.get_thread_by_slug!(forum, conn.assigns[:visible_forums], conn.params["slug"])
       |> Threads.reject_deleted_threads(conn.assigns[:view_all])
+      |> Threads.build_message_tree(ConfigManager.uconf(conn, "sort_messages"))
 
     message = Messages.get_message_from_mid!(thread, conn.params["message_id"])
 
