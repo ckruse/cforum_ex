9f9171b8d25edc28789d76e691384038bc9e552c
Christian Kruse <cjk@defunct.ch>
2019-07-14 13:59:29 +0200

    fix: apply manual fixes to preview as well
    fixes #43

--- assets/js/components/editor/live_preview.js
+++ assets/js/components/editor/live_preview.js
@@ -14,14 +14,13 @@ class LivePreview extends React.Component {
   }
 
   render() {
+    const fixedContent = CfMarkdown.manualFixes(this.props.content);
+
     return (
       <article className="cf-thread-message cf-preview">
         <h3>{t("preview")}</h3>
 
-        <div
-          className="cf-posting-content"
-          dangerouslySetInnerHTML={{ __html: this.mdparser.render(this.props.content) }}
-        />
+        <div className="cf-posting-content" dangerouslySetInnerHTML={{ __html: this.mdparser.render(fixedContent) }} />
       </article>
     );
   }
