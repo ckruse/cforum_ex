272401a42695529fd6186f1e5119d672e2c0af46
Christian Kruse <cjk@defunct.ch>
2019-07-30 14:44:06 +0200

    ref: better readable code

--- lib/cforum_web/controllers/archive_controller.ex
+++ lib/cforum_web/controllers/archive_controller.ex
@@ -21,34 +21,35 @@ defmodule CforumWeb.ArchiveController do
   end
 
   def threads(conn, %{"year" => year, "month" => month_name} = params) do
-    with {:ok, month} <- NaiveDateTime.new(String.to_integer(year), Timex.month_to_num(month_name), 1, 0, 0, 0) do
-      start_date = Timex.beginning_of_month(month)
-      end_date = Timex.end_of_month(month)
-      page = Paginator.parse_page(params["p"]) - 1
-      limit = ConfigManager.uconf(conn, "pagination", :int)
+    case NaiveDateTime.new(String.to_integer(year), Timex.month_to_num(month_name), 1, 0, 0, 0) do
+      {:ok, month} ->
+        start_date = Timex.beginning_of_month(month)
+        end_date = Timex.end_of_month(month)
+        page = Paginator.parse_page(params["p"]) - 1
+        limit = ConfigManager.uconf(conn, "pagination", :int)
 
-      threads =
-        conn.assigns[:current_forum]
-        |> Archive.list_archived_threads(conn.assigns[:visible_forums], start_date, end_date)
-        |> Threads.reject_deleted_threads(conn.assigns[:view_all])
-        |> Threads.apply_user_infos(conn.assigns[:current_user],
-          close_read_threads: ConfigManager.uconf(conn, "open_close_close_when_read") == "yes",
-          open_close_default_state: ConfigManager.uconf(conn, "open_close_default")
-        )
-        |> Threads.apply_highlights(conn)
+        threads =
+          conn.assigns[:current_forum]
+          |> Archive.list_archived_threads(conn.assigns[:visible_forums], start_date, end_date)
+          |> Threads.reject_deleted_threads(conn.assigns[:view_all])
+          |> Threads.apply_user_infos(conn.assigns[:current_user],
+            close_read_threads: ConfigManager.uconf(conn, "open_close_close_when_read") == "yes",
+            open_close_default_state: ConfigManager.uconf(conn, "open_close_default")
+          )
+          |> Threads.apply_highlights(conn)
 
-      count = length(threads)
+        count = length(threads)
 
-      threads =
-        threads
-        |> Threads.sort_threads(ConfigManager.uconf(conn, "sort_threads"))
-        |> Threads.paged_thread_list(page, limit)
-        |> Threads.build_message_trees(ConfigManager.uconf(conn, "sort_messages"))
+        threads =
+          threads
+          |> Threads.sort_threads(ConfigManager.uconf(conn, "sort_threads"))
+          |> Threads.paged_thread_list(page, limit)
+          |> Threads.build_message_trees(ConfigManager.uconf(conn, "sort_messages"))
 
-      p = Paginator.paginate(count, per_page: limit, page: page + 1)
+        p = Paginator.paginate(count, per_page: limit, page: page + 1)
 
-      render(conn, "threads.html", threads: threads, page: p, start_date: start_date)
-    else
+        render(conn, "threads.html", threads: threads, page: p, start_date: start_date)
+
       _ ->
         conn
         |> put_status(:not_found)
