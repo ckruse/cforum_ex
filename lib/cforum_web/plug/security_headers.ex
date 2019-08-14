6ce1585036c3289f6afe5f1a4e8f1bb2e7189f16
Christian Kruse <cjk@defunct.ch>
2019-08-14 21:57:06 +0200

    ref: more CSP fixes, this time for safari

--- lib/cforum_web/plug/security_headers.ex
+++ lib/cforum_web/plug/security_headers.ex
@@ -13,18 +13,38 @@ defmodule CforumWeb.Plug.SecurityHeaders do
         |> :crypto.strong_rand_bytes()
         |> Base.url_encode64(padding: false)
 
-      script_csp = "script-src 'self' 'nonce-#{js_nonce}'"
+      script_csp = "script-src 'self' 'nonce-#{js_nonce}' *.selfhtml.org"
       # 'nonce-#{style_nonce}' not yet possible, due to mathjax
       style_csp = "style-src 'self' 'unsafe-inline'"
+      connect_csp = "connect-src 'self' #{scheme()}://#{CforumWeb.Endpoint.config(:url)[:host]}#{port()}"
 
       conn
       |> Plug.Conn.assign(:nonce_for_js, js_nonce)
       |> Plug.Conn.assign(:nonce_for_style, style_nonce)
-      |> Plug.Conn.put_resp_header("Content-Security-Policy", "default-src 'self'; #{script_csp}; #{style_csp}")
+      |> Plug.Conn.put_resp_header(
+        "Content-Security-Policy",
+        "default-src 'self'; #{script_csp}; #{style_csp}; #{connect_csp}"
+      )
     else
       conn
       |> Plug.Conn.assign(:nonce_for_js, "")
       |> Plug.Conn.assign(:nonce_for_style, "")
     end
+  end
+
+  defp scheme do
+    port = CforumWeb.Endpoint.config(:http)[:port]
+
+    if port == 443,
+      do: "wss",
+      else: "ws"
+  end
+
+  defp port do
+    port = CforumWeb.Endpoint.config(:http)[:port]
+
+    if port != 443 && port != 80,
+      do: ":#{port}",
+      else: ""
   end
 end
