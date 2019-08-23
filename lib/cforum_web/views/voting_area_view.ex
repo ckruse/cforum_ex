f515ea0581db3ef4dbde6e2a4572abfea683c7e8
Christian Kruse <cjk@defunct.ch>
2019-08-23 10:47:58 +0200

    fix: wrong path for SVG

--- lib/cforum_web/views/voting_area_view.ex
+++ lib/cforum_web/views/voting_area_view.ex
@@ -16,7 +16,7 @@ defmodule CforumWeb.VotingAreaView do
     [
       {:safe,
        "<svg width=\"150\" height=\"150\" viewBox=\"0 0 150 150\" xmlns=\"http://www.w3.org/2000/svg\"><use xlink:href=\""},
-      Routes.static_path(conn, "/images/cforum-voting-area-icons.svg"),
+      Routes.static_path(conn, "/images/voting-area-icons.svg"),
       "#",
       path,
       {:safe, "\"></use></svg>"}
