d0eb9659af5d68555e8cf070d2dd5c5dfe2187fc
Christian Kruse <cjk@defunct.ch>
2019-12-21 20:59:46 +0100

    fix: ensure that the year is numeric

--- lib/cforum_web/controllers/admin/advent_calendar_controller.ex
+++ lib/cforum_web/controllers/admin/advent_calendar_controller.ex
@@ -12,6 +12,9 @@ defmodule CforumWeb.Admin.AdventCalendarController do
   end
 
   def index(conn, %{"year" => year}) do
+    if !Regex.match?(~r/^\d+$/, year),
+      do: raise(Cforum.Errors.NotFoundError, conn: conn)
+
     advent_calendar_days = AdventCalendars.list_advent_calendar_days(year)
     render(conn, "index.html", advent_calendar_days: advent_calendar_days, year: year)
   end
--- lib/cforum_web/controllers/advent_controller.ex
+++ lib/cforum_web/controllers/advent_controller.ex
@@ -4,6 +4,9 @@ defmodule CforumWeb.AdventController do
   alias Cforum.AdventCalendars
 
   def index(conn, %{"year" => year}) do
+    if !Regex.match?(~r/^\d+$/, year),
+      do: raise(Cforum.Errors.NotFoundError, conn: conn)
+
     advent_calendar_days = AdventCalendars.list_advent_calendar_days(year)
     render(conn, "index.html", advent_calendar_days: advent_calendar_days, year: year, today: Timex.today())
   end
