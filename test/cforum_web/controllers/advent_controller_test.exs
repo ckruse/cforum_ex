8be2eeeeb0fea7870e9e958d71fcf492e1b3f2d8
Christian Kruse <cjk@defunct.ch>
2019-12-22 09:36:05 +0100

    ref: tests for advent controller

---
+++ test/cforum_web/controllers/advent_controller_test.exs
@@ -0,0 +1,12 @@
+defmodule CforumWeb.DayControllerTest do
+  use CforumWeb.ConnCase
+
+  alias CforumWeb.Router.Helpers, as: Routes
+
+  describe "index" do
+    test "lists all advent_calendar_days", %{conn: conn} do
+      conn = get(conn, Routes.advent_path(conn, :index, "2019"))
+      assert html_response(conn, 200) =~ gettext("advent calendar %{year}", year: "2019")
+    end
+  end
+end
