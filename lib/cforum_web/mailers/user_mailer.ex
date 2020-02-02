37a05a812d268f43511310c26dccfd4be68eeee7
Christian Kruse <cjk@defunct.ch>
2020-02-02 19:34:36 +0100

    ref: pluralize years

--- lib/cforum_web/mailers/user_mailer.ex
+++ lib/cforum_web/mailers/user_mailer.ex
@@ -18,11 +18,21 @@ defmodule CforumWeb.UserMailer do
     |> render_body(:reset_password_mail, %{user: user})
   end
 
+  @years [
+    gettext("since one year"),
+    gettext("since two years"),
+    gettext("since three years"),
+    gettext("since four years"),
+    gettext("since five years")
+  ]
+
   def inactivity_mail(user, years) do
+    years_str = Enum.at(@years, years - 1, gettext("since %{years} year(s)", years: years))
+
     new()
     |> from(Application.get_env(:cforum, :mail_sender, "cforum@example.org"))
     |> to({user.username, user.email})
     |> subject(gettext("inactivity warning"))
-    |> render_body(:inactivity_warning_mail, %{user: user, years: years})
+    |> render_body(:inactivity_warning_mail, %{user: user, years: years_str})
   end
 end
