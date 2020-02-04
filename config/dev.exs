49eaa0b8cbe15a1197bca90f65117ce086180d34
Christian Kruse <cjk@defunct.ch>
2020-02-04 20:00:55 +0100

    ref: update config to phoenix 4.0 standard

--- config/dev.exs
+++ config/dev.exs
@@ -38,6 +38,7 @@ config :logger, :console, format: "[$level] $message\n"
 # Set a higher stacktrace during development. Avoid configuring such
 # in production as building large stacktraces may be expensive.
 config :phoenix, :stacktrace_depth, 20
+config :phoenix, :plug_init_mode, :runtime
 
 # Configure your database
 config :cforum, Cforum.Repo,
