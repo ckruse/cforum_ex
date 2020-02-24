3ee7610f4f7ade6646555ddf46ab4211e346ba19
Christian Kruse <cjk@defunct.ch>
2020-02-24 07:02:20 +0100

    fix: typo

--- config/config.exs
+++ config/config.exs
@@ -43,7 +43,7 @@ config :cforum, Oban,
     {"0 2 * * *", Cforum.Jobs.AuditingCleanupJob},
     {"0 5 * * *", Cforum.Jobs.YearlingBadgeDistributorJob},
     {"0 3 1 * *", Cforum.Jobs.DatabaseMaintenanceJob},
-    {"4 17 * * *", Cforum.Jobs.SendInactivityNotificationMailJob}
+    {"17 4 * * *", Cforum.Jobs.SendInactivityNotificationMailJob}
   ]
 
 config :cforum, ObanWeb, repo: Cforum.Repo, verbose: false
