e2cc3db444e40035ce401fb879ca108ab75879d6
Christian Kruse <cjk@defunct.ch>
2020-04-12 07:58:00 +0200

    update dependencies

--- mix.exs
+++ mix.exs
@@ -37,6 +37,7 @@ defmodule Cforum.Mixfile do
       {:phoenix_pubsub, "~> 1.0"},
       {:ecto_sql, "~> 3.0"},
       {:phoenix_ecto, "~> 4.0"},
+      {:ecto, "~> 3.4.0"},
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
