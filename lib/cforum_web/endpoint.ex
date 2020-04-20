3e5a967cb148093648dcce910de1f145da1c25ed
Christian Kruse <cjk@defunct.ch>
2020-04-20 19:48:03 +0200

    fix: set HttpOnly on session cookies

--- lib/cforum_web/endpoint.ex
+++ lib/cforum_web/endpoint.ex
@@ -49,7 +49,7 @@ defmodule CforumWeb.Endpoint do
   # The session will be stored in the cookie and signed,
   # this means its contents can be read but not tampered with.
   # Set :encryption_salt if you would also like to encrypt it.
-  plug(Plug.Session, store: :cookie, key: "_cforum_key", signing_salt: "aRQT3uvM")
+  plug(Plug.Session, store: :cookie, key: "_cforum_key", signing_salt: "aRQT3uvM", http_only: true)
 
   plug(CforumWeb.Router)
 
