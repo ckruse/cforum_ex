4f8ec02b10f13baf7379a9f3d163d2c2103508d1
Christian Kruse <cjk@defunct.ch>
2020-04-21 11:14:32 +0200

    fix: there are more actions than just :new

--- lib/cforum_web/views/users/registration_view.ex
+++ lib/cforum_web/views/users/registration_view.ex
@@ -5,8 +5,12 @@ defmodule CforumWeb.Users.RegistrationView do
   alias CforumWeb.Views.ViewHelpers.Path
   alias CforumWeb.ErrorHelpers
 
-  def page_title(:new, _), do: gettext("register")
-  def page_heading(:new, _), do: gettext("register")
-  def body_id(:new, _), do: "registrations-new"
-  def body_classes(:new, _), do: "registrations new"
+  def page_title(action, _) when action in [:new, :create], do: gettext("register")
+  def page_title(:confirm, _), do: gettext("Confirm my account")
+
+  def page_heading(action, _) when action in [:new, :create], do: gettext("register")
+  def page_heading(:confirm, _), do: gettext("Confirm my account")
+
+  def body_id(action, _), do: "registrations-#{action}"
+  def body_classes(action, _), do: "registrations #{action}"
 end
