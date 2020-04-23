92b66367b467b368da8e2056d02549f16ad11918
Christian Kruse <cjk@defunct.ch>
2020-04-23 15:52:16 +0200

    version 5.3.77

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Cforum.Mixfile do
   def project do
     [
       app: :cforum,
-      version: "5.3.76",
+      version: "5.3.77",
       elixir: "~> 1.8",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
