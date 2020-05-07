f586981469c6e30588d2eead6baf5bc21668af29
Christian Kruse <cjk@defunct.ch>
2020-05-07 21:16:17 +0200

    fix: parse() now requires a format argument; use parseJSON instead
    fixes #151

--- assets/js/cleanup.js
+++ assets/js/cleanup.js
@@ -1,16 +1,16 @@
-import { parse, isBefore, addDays } from "date-fns";
+import { isBefore, addDays, parseJSON } from "date-fns";
 
 const MAX_DRAFT_AGE = 14;
 
 const cleanupLocalstorage = () => {
   const keys = Object.keys(window.localStorage);
-  keys.forEach(key => {
+  keys.forEach((key) => {
     if (!key.match(/_draft$/)) {
       return;
     }
 
     const draft = JSON.parse(localStorage.getItem(key));
-    if (draft.saved && !isBefore(new Date(), addDays(parse(draft.saved), MAX_DRAFT_AGE))) {
+    if (draft.saved && !isBefore(new Date(), addDays(parseJSON(draft.saved), MAX_DRAFT_AGE))) {
       localStorage.removeItem(key);
     }
   });
