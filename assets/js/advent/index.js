d417f6d4e9835c4c99ac93be9c4584487b2f2a11
Christian Kruse <cjk@defunct.ch>
2020-10-14 20:58:21 +0200

    ref: nicer code

--- assets/js/advent/index.js
+++ assets/js/advent/index.js
@@ -1,9 +1,9 @@
-if (document.body.dataset.controller === "AdventController" && document.body.dataset.action === "show") {
-  import(/* webpackChunkName: "advent" */ "./advent.js");
-}
-
 const ADMIN_ACTIONS = ["new", "create", "edit", "update"];
 const { controller, action } = document.body.dataset;
+
+if (controller === "AdventController" && action === "show") {
+  import(/* webpackChunkName: "advent" */ "./advent.js");
+}
 
 if (controller === "Admin.AdventCalendarController" && ADMIN_ACTIONS.includes(action)) {
   import(/* webpackChunkName: "advent" */ "./admin.js");
