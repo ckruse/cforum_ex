ee184c245184621e4496141530172410a019ada9
Christian Kruse <cjk@defunct.ch>
2020-11-20 19:49:05 +0100

    ref: updated frontend key

--- assets/js/appsignal.js
+++ assets/js/appsignal.js
@@ -4,7 +4,7 @@ import { plugin } from "@appsignal/plugin-window-events";
 
 import { t } from "./modules/i18n";
 
-const appsignal = new Appsignal({ key: "fce1cc45-b503-4457-af8a-608be9f58f49" });
+const appsignal = new Appsignal({ key: "7f5fcb04-c262-4b2e-b383-9ce2a7b4a821" });
 appsignal.use(plugin);
 
 export const FallbackComponent = () => <div>{t("Oops, something went wrong!")}</div>;
