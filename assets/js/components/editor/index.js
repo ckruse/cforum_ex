11fab05f9e223e2d19c93872309297ef625a9cad
Christian Kruse <cjk@defunct.ch>
2020-12-06 07:48:34 +0100

    ref: use React hooks and a function component

--- assets/js/components/editor/index.js
+++ assets/js/components/editor/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { ErrorBoundary } from "@appsignal/react";
 
 import DefaultReplacements from "./default_replacements";
@@ -14,54 +14,57 @@ import { replaceAt, getSelection, escapeText } from "./helpers";
 import AutocompleteTextarea from "../autocomplete";
 import appsignal, { FallbackComponent } from "../../appsignal";
 
-class CfEditor extends React.Component {
-  textarea = React.createRef();
+export default function CfEditor({ text, onChange, id, name, errors, withImages }) {
+  const [value, setValue] = useState(text);
+  const [dragging, setDragging] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [didPosition, setDidPosition] = useState(false);
+  const textarea = useRef();
 
-  constructor(props) {
-    super(props);
+  useEffect(() => {
+    setValue(text);
+  }, [text]);
 
-    this.state = { value: props.text, dragging: false, loading: false };
+  useEffect(
+    () => {
+      if (textarea.current && !didPosition) {
+        textarea.current.focus();
+        const found = value.indexOf("\n\n");
+        const pos = found === -1 ? 0 : found + 2;
+        textarea.current.selectionStart = textarea.current.selectionEnd = pos;
+        setDidPosition(true);
+      }
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [textarea.current]
+  );
+
+  function valueChanged(ev) {
+    setValue(ev.target.value);
   }
 
-  componentDidUpdate(prevProps) {
-    if (prevProps.text !== this.props.text) {
-      this.setState({ value: this.props.text });
-    }
-  }
+  function doSetValue(value, opts = {}) {
+    setValue(value);
 
-  componentDidMount() {
-    this.textarea.current.focus();
-    const found = this.state.value.indexOf("\n\n");
-    const pos = found === -1 ? 0 : found + 2;
-    this.textarea.current.selectionStart = this.textarea.current.selectionEnd = pos;
-  }
-
-  valueChanged = (ev) => {
-    this.setValue(ev.target.value);
-  };
-
-  setValue = (value, opts = {}) => {
-    this.setState({ value });
-
-    if (this.props.onChange) {
-      this.props.onChange(value);
+    if (onChange) {
+      onChange(value);
     }
 
     if (opts.start && opts.end) {
       window.setTimeout(() => {
-        this.textarea.current.selectionStart = opts.start;
-        this.textarea.current.selectionEnd = opts.end;
+        textarea.current.selectionStart = opts.start;
+        textarea.current.selectionEnd = opts.end;
       }, 0);
     }
-  };
+  }
 
-  dragStart = () => {
-    this.setState({ dragging: true });
-  };
-  dragStop = () => {
-    this.setState({ dragging: false });
-  };
-  fileDropped = async (file, desc, title) => {
+  function dragStart() {
+    setDragging(true);
+  }
+  function dragStop() {
+    setDragging(false);
+  }
+  async function fileDropped(file, desc, title) {
     const fdata = new FormData();
     fdata.append("image", file);
 
@@ -73,13 +76,14 @@ class CfEditor extends React.Component {
     });
 
     const json = await rsp.json();
-    this.fileUploadFinished(json, desc, title, file);
-  };
+    fileUploadFinished(json, desc, title, file);
+  }
 
-  fileUploadFinished = (rsp, desc, title, file) => {
-    this.setState({ loading: false });
+  function fileUploadFinished(rsp, desc, title, file) {
+    setLoading(false);
+
     if (rsp.status === "success") {
-      const { start, end } = getSelection(this.textarea.current);
+      const { start, end } = getSelection(textarea.current);
       const size = file.type === "image/svg+xml" ? "" : "?size=medium";
       const escapedTitle = title ? ' "' + escapeText(title, '"') + '"' : "";
       const escapedDesc = escapeText(desc, "\\]");
@@ -87,67 +91,62 @@ class CfEditor extends React.Component {
 
       const image = `[![${escapedDesc}](${escapedLocation}${size}${escapedTitle})](${escapeText(rsp.location, ")")})`;
 
-      const value = replaceAt(this.state.value, image, start, end);
-      this.setState({ value });
-      this.textarea.current.selectionStart = start;
-      this.textarea.current.selectionEnd = start + image.length;
-      this.textarea.current.focus();
+      const newValue = replaceAt(value, image, start, end);
+      setValue(newValue);
+      textarea.current.selectionStart = start;
+      textarea.current.selectionEnd = start + image.length;
+      textarea.current.focus();
     } else {
       alertError(t("Oops, something went wrong!"));
     }
-  };
-
-  render() {
-    const { id, name, errors } = this.props;
-    let className = "cf-cgroup cf-textarea-only cf-editor";
-    if (this.state.dragging) {
-      className += " dragging";
-    }
-
-    if (errors[id]) {
-      className += " has-error";
-    }
-
-    return (
-      <ErrorBoundary instance={appsignal} fallback={(error) => <FallbackComponent />}>
-        <fieldset>
-          <label htmlFor={id}>
-            {t("posting text")}{" "}
-            {errors[id] && (
-              <>
-                <span className="help error">{errors[id]}</span>
-              </>
-            )}
-          </label>
-
-          <div className={className}>
-            <Toolbar
-              value={this.state.value}
-              changeValue={this.setValue}
-              textarea={this.textarea}
-              onImageUpload={this.fileDropped}
-              enableImages={this.props.withImages}
-            />
-
-            <AutocompleteTextarea
-              name={name}
-              value={this.state.value}
-              onChange={this.valueChanged}
-              onComplete={this.setValue}
-              triggers={[DefaultReplacements, EmojiReplacements, SmileyReplacements, MentionsReplacements]}
-              ref={this.textarea}
-            />
-          </div>
-
-          {this.props.withImages && (
-            <Dropzone onDragStart={this.dragStart} onDragStop={this.dragStop} onDrop={this.fileDropped} />
-          )}
-
-          <LivePreview content={this.state.value} />
-        </fieldset>
-      </ErrorBoundary>
-    );
   }
-}
 
-export default CfEditor;
+  let className = "cf-cgroup cf-textarea-only cf-editor";
+  if (dragging) {
+    className += " dragging";
+  }
+
+  if (errors[id]) {
+    className += " has-error";
+  }
+
+  return (
+    <ErrorBoundary instance={appsignal} fallback={(error) => <FallbackComponent />}>
+      <fieldset>
+        <label htmlFor={id}>
+          {t("posting text")}{" "}
+          {errors[id] && (
+            <>
+              <span className="help error">{errors[id]}</span>
+            </>
+          )}
+        </label>
+
+        <div className={className}>
+          <Toolbar
+            value={value}
+            changeValue={doSetValue}
+            textarea={textarea}
+            onImageUpload={fileDropped}
+            enableImages={withImages}
+          />
+
+          <AutocompleteTextarea
+            name={name}
+            value={value}
+            onChange={valueChanged}
+            onComplete={doSetValue}
+            triggers={[DefaultReplacements, EmojiReplacements, SmileyReplacements, MentionsReplacements]}
+            ref={textarea}
+          />
+        </div>
+
+        {withImages && (
+          <Dropzone onDragStart={dragStart} onDragStop={dragStop} onDrop={fileDropped} loading={loading} />
+        )}
+
+        <LivePreview content={value} />
+      </fieldset>
+    </ErrorBoundary>
+  );
+}
