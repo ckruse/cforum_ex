9d612e0df39b54357635c9b738e38ad2b66e9263
Christian Kruse <cjk@defunct.ch>
2020-12-25 07:44:11 +0100

    this doesn’t work like I expected…

--- lib/cforum/application.ex
+++ lib/cforum/application.ex
@@ -10,7 +10,6 @@ defmodule Cforum.Application do
     children = [
       # Start the Ecto repository
       Cforum.Repo,
-      Cforum.Release,
       # Start the Telemetry supervisor
       CforumWeb.Telemetry,
       # Start the PubSub system
