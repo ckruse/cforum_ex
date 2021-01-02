7ec501512064d55a3e0ffe2ca5ab97f78fcdb985
Christian Kruse <cjk@defunct.ch>
2021-01-02 13:39:33 +0100

    fix: meta.reason doesn’t exist

--- lib/cforum/jobs/appsignal.ex
+++ lib/cforum/jobs/appsignal.ex
@@ -2,7 +2,7 @@ defmodule Cforum.Jobs.Appsignal do
   require Logger
 
   def handle_event([:oban, :job, :exception], _measurement, meta, _) do
-    Logger.error("Error executing job: #{meta.kind} (#{inspect(meta.reason)})\n\n#{inspect(meta.stacktrace)}")
+    Logger.error("Error executing job: #{meta.kind} (#{inspect(meta.error)})\n\n#{inspect(meta.stacktrace)}")
 
     if meta.attempt >= meta.max_attempts,
       do: Appsignal.send_error(meta[:kind], meta[:error], meta[:stacktrace])
