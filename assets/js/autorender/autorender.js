fa0210c7f0224995512ad67277bec7871ed935cc
Christian Kruse <cjk@defunct.ch>
2021-03-28 15:00:01 +0200

    make automatically rendered editor components more configurable

--- assets/js/autorender/autorender.js
+++ assets/js/autorender/autorender.js
@@ -6,8 +6,24 @@ import CfEditor from "../components/editor";
 const setupEditors = (nodes) => {
   nodes.forEach((el) => {
     const area = el.querySelector("textarea");
+    const mentions = el.dataset.mentions == "yes" || false;
+    const images = el.dataset.images == "yes";
+    const preview = el.dataset.preview != "no";
+    const counter = el.dataset.counter != "no";
 
-    render(<CfEditor text={area.value} name={area.name} id={area.id} mentions={false} errors={{}} />, el);
+    render(
+      <CfEditor
+        text={area.value}
+        name={area.name}
+        id={area.id}
+        withMentions={mentions}
+        withImages={images}
+        withPreview={preview}
+        withCounter={counter}
+        errors={{}}
+      />,
+      el
+    );
   });
 };
 
