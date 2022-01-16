ca77b507cd196bdf782a5e3d9564e0203405d72e
Christian Kruse <cjk@defunct.ch>
2022-01-16 20:22:05 +0100

    refactoring the image deletion job to be more reliable

--- lib/cforum/jobs/clean_images_job.ex
+++ lib/cforum/jobs/clean_images_job.ex
@@ -5,21 +5,46 @@ defmodule Cforum.Jobs.CleanImagesJob do
 
   alias Cforum.Repo
 
+  alias Cforum.Helpers.AsyncHelper
+
   alias Cforum.Media
   alias Cforum.Media.Image
+  alias Cforum.Messages.Message
+
+  @limit 100
 
   def perform(_) do
-    Repo.transaction(
-      fn ->
-        from(image in Image,
-          left_join: msg in assoc(image, :messages),
-          where: is_nil(msg.message_id),
-          where: image.created_at < fragment("NOW() - INTERVAL '14 days'")
-        )
-        |> Repo.stream()
-        |> Enum.each(&Media.delete_image(&1, nil))
-      end,
-      timeout: :infinity
-    )
+    delete_images(0)
+
+    :ok
+  end
+
+  defp delete_images(offset) do
+    images =
+      from(image in Image,
+        left_join: msg in assoc(image, :messages),
+        where: is_nil(msg.message_id),
+        where: image.created_at < fragment("NOW() - INTERVAL '14 days'"),
+        limit: @limit,
+        offset: ^offset
+      )
+      |> Repo.all()
+
+    AsyncHelper.run_async(fn ->
+      Enum.each(images, &delete_image/1)
+    end)
+
+    if images != [],
+      do: delete_images(offset + @limit)
+  end
+
+  # just to be absolutely sure that we don't delete images that are still referenced we check again if the image
+  # name is referenced somewhere in message contents
+  defp delete_image(img) do
+    fname = "%/images/#{img.filename}%"
+    exists = from(msg in Message, where: like(msg.content, ^fname)) |> Repo.exists?()
+
+    if !exists,
+      do: Media.delete_image(img, nil)
   end
 end
